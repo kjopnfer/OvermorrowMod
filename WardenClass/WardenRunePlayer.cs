1b9a44bc57a253d72a8a4c4cb49884f606dbdc40
Justin Lai <gaypal0805@gmail.com>
2021-05-21 11:03:50 -0400

    Added despawn animation for runic circles

--- WardenClass/WardenRunePlayer.cs
+++ WardenClass/WardenRunePlayer.cs
@@ -40,12 +40,12 @@ namespace OvermorrowMod.WardenClass
         public override void ResetEffects()
         {
             ActiveRune = false;
-            RuneID = 0;
+            //RuneID = 0;
         }
 
         public override void ModifyDrawLayers(List<PlayerLayer> layers)
         {
-            if (ActiveRune)
+            if (RuneID != 0)
             {
                 int body = layers.FindIndex(l => l == PlayerLayer.MiscEffectsBack);
                 if (body < 0)
@@ -59,24 +59,47 @@ namespace OvermorrowMod.WardenClass
 
         public int runeCounter;
         public int rotateCounter;
+        public bool runeDeactivate = false;
         public static readonly PlayerLayer Rune = new PlayerLayer("OvermorrowMod", "Body", delegate (PlayerDrawInfo drawInfo)
         {
             Player drawPlayer = drawInfo.drawPlayer;
             Mod mod = ModLoader.GetMod("OvermorrowMod");
             WardenRunePlayer modPlayer = drawPlayer.GetModPlayer<WardenRunePlayer>();
-
-            // Probably use an enum or something
-            if (modPlayer.RuneID == 0)
+            
+            if (modPlayer.runeDeactivate && modPlayer.runeCounter == 0)
             {
                 modPlayer.ActiveRune = false;
                 modPlayer.runeCounter = 0;
                 modPlayer.rotateCounter = 0;
+                modPlayer.runeDeactivate = false;
+                modPlayer.RuneID = 0;
                 return;
             }
 
-            if (modPlayer.runeCounter < 300)
+            if (modPlayer.RuneID == 0)
             {
-                modPlayer.runeCounter++;
+                return;
+            }
+
+            // Probably use an enum or something
+            if (!modPlayer.ActiveRune && !modPlayer.runeDeactivate)
+            {
+                modPlayer.runeDeactivate = true;
+            }
+
+            if (!modPlayer.runeDeactivate)
+            {
+                if (modPlayer.runeCounter < 300)
+                {
+                    modPlayer.runeCounter++;
+                }
+            }
+            else
+            {
+                if (modPlayer.runeCounter > 0)
+                {
+                    modPlayer.runeCounter--;
+                }
             }
 
             modPlayer.rotateCounter++;
@@ -107,14 +130,17 @@ namespace OvermorrowMod.WardenClass
                 case 7: // Thorns of the Jungle
                     symbolTexture = ModContent.GetTexture("OvermorrowMod/WardenClass/RuneCircles/temp");
                     break;
+                default:
+                    symbolTexture = ModContent.GetTexture("OvermorrowMod/WardenClass/RuneCircles/temp");
+                    break;
             }
             
             //int num24 = (int)(((float)(drawPlayer.miscCounter / 300.0)).ToRotationVector2().Y * 4.0);
             Vector2 position = new Vector2((int)(drawPlayer.position.X - (double)Main.screenPosition.X - (drawPlayer.bodyFrame.Width / 2) + (drawPlayer.width / 2)), (int)(drawPlayer.position.Y - (double)Main.screenPosition.Y + drawPlayer.height - drawPlayer.bodyFrame.Height + 4.0)) + drawPlayer.bodyPosition + new Vector2((drawPlayer.bodyFrame.Width / 2), (drawPlayer.bodyFrame.Height / 2)) + new Vector2((-drawPlayer.direction), 0);
 
             // Replaced drawPlayer.miscCounter with modPlayer.symbolCounter, there might be syncing issues idk
-            double deg = (modPlayer.rotateCounter * 1.2) * MathHelper.Lerp(1, 4, (float)(modPlayer.runeCounter / 300.0));
 
+            double deg = (modPlayer.rotateCounter * 1.2) * MathHelper.Lerp(1, 4, (float)(!modPlayer.runeDeactivate ? modPlayer.runeCounter / 300.0 : 1));
             float rad = (float)(deg * (Math.PI / 180));
 
             float scale = (float)((modPlayer.runeCounter * 2 >= 300 ? 300 : modPlayer.runeCounter * 2) / 300.0) * 1.5f;
