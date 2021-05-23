cb6dc02e61cbed0c151235dc35fccf424ab5baea
Justin Lai <gaypal0805@gmail.com>
2021-05-23 17:52:35 -0400

    Added Granite book

---
+++ Items/Weapons/PreHardmode/Magic/GraniteBook.cs
@@ -0,0 +1,38 @@
+using Microsoft.Xna.Framework;
+using OvermorrowMod.Projectiles.Magic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Items.Weapons.PreHardmode.Magic
+{
+    public class GraniteBook : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Granite Lance");
+            Tooltip.SetDefault("Launches a Granite Spike that ignore 5 defense");
+        }
+
+        public override void SetDefaults()
+        {
+            item.autoReuse = true;
+            item.rare = ItemRarityID.Green;
+            item.mana = 11;
+            item.UseSound = SoundID.Item20;
+            item.noMelee = true;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.damage = 18;
+            item.useTurn = false;
+            item.useAnimation = 28;
+            item.useTime = 28;
+            item.width = 28;
+            item.height = 30;
+            item.shoot = ModContent.ProjectileType<GraniteSpike>();
+            item.shootSpeed = 12f;
+            item.knockBack = 3f;
+            item.magic = true;
+            item.value = Item.sellPrice(gold: 1);
+        }
+    }
+}
---
+++ Projectiles/Magic/GraniteSpike.cs
@@ -0,0 +1,58 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Projectiles.Magic
+{
+    public class GraniteSpike : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Granite Spike");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.friendly = true;
+            projectile.hostile = false;
+            projectile.penetrate = 2;
+            projectile.timeLeft = 600;
+            projectile.tileCollide = true;
+            projectile.magic = true;
+        }
+
+        public override void AI()
+        {
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90f);
+           
+            float num116 = 16f;
+            for (int num117 = 0; (float)num117 < 16; num117++)
+            {
+                Vector2 spinningpoint7 = Vector2.UnitX * 0f;
+                spinningpoint7 += -Vector2.UnitY.RotatedBy((float)num117 * ((float)Math.PI * 2f / num116)) * new Vector2(1f, 4f);
+                spinningpoint7 = spinningpoint7.RotatedBy(projectile.velocity.ToRotation());
+                Vector2 position = projectile.Center;
+                Dust dust = Terraria.Dust.NewDustPerfect(position, 63, new Vector2(0f, 0f), 0, new Color(0, 242, 255), 1f);
+                dust.noLight = true;
+                dust.noGravity = true;
+                dust.position = projectile.Center + spinningpoint7;
+            }
+
+        }
+
+        public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            Main.player[projectile.owner].armorPenetration += 5;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Main.player[projectile.owner].armorPenetration -= 5;
+        }
+    }
+}
