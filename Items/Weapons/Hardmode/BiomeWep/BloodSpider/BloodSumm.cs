e8db4a14e053324daffb3a9043cbd1f5f7acb1ca
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-06-18 19:27:52 -0400

    added biome weapons
    all

---
+++ Items/Weapons/Hardmode/BiomeWep/BloodSpider/BloodSumm.cs
@@ -0,0 +1,122 @@
+﻿using OvermorrowMod.Items.Weapons.Hardmode.BiomeWep.BloodSpider;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+
+namespace OvermorrowMod.Items.Weapons.Hardmode.BiomeWep.BloodSpider
+{
+    public class BloodSumm : ModProjectile
+    {
+
+        private int movement = 0;
+        private int timer = 60;
+        private int timer2 = 0;
+        private int timer3 = 0;
+        private int movement2 = 0;
+        private int penet = 0;
+        private int penet2 = 0;
+        private int savedDMG = 0;
+
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.Raven);
+            aiType = ProjectileID.Raven;
+            projectile.netImportant = true;
+            projectile.minion = true;
+            projectile.minionSlots = 1.5f;
+            projectile.penetrate = -1;
+        }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blood Spider");
+            Main.projFrames[base.projectile.type] = 8;
+        }
+        public override void AI()
+        {
+
+
+            Player player = Main.player[projectile.owner];
+
+            #region Active check
+            if (player.dead || !player.active)
+            {
+                player.ClearBuff(ModContent.BuffType<BloodCrawlerBuff>());
+            }
+            if (player.HasBuff(ModContent.BuffType<BloodCrawlerBuff>()))
+            {
+                projectile.timeLeft = 2;
+            }
+            #endregion
+
+
+
+			if (player.channel) 
+            {
+			timer++;
+			if(timer == 62)
+			{
+
+                Vector2 position = projectile.Center;
+                Vector2 targetPosition = Main.MouseWorld;
+                Vector2 direction = targetPosition - position;
+                direction.Normalize();
+                Vector2 newpoint2 = new Vector2(direction.X,  direction.Y).RotatedByRandom(MathHelper.ToRadians(1.5f));
+                float speed = 20f;
+                Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Items/Hork"), projectile.position);
+                Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, newpoint2.X * speed, newpoint2.Y * speed, mod.ProjectileType("RottingEgg"), projectile.damage + 5, 1f, projectile.owner, 0f);
+			    timer = 0;
+			}
+
+            if (Main.player[projectile.owner].Center.X > projectile.Center.X)
+            {
+                projectile.spriteDirection = -1;
+            }
+            }
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            projectile.height = 80;
+            projectile.width = 80;
+
+            projectile.minion = true;
+            projectile.minionSlots = 1.5f;
+
+            projectile.height = 37;
+
+
+
+
+            timer3++;
+            if(timer3 == 1)
+            {
+                savedDMG = projectile.damage;
+            }
+
+            if(penet > 0)
+            {
+			    projectile.damage = 0;
+                penet++;
+            }
+            
+            if(projectile.damage == 0)
+            {
+                penet2++;
+            }
+
+            if(penet2 > 34)
+            {
+                penet = 0;
+                penet2 = 0;
+                projectile.damage = savedDMG;
+            }
+        }
+
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            penet++;
+            projectile.velocity.X *= 4;
+            projectile.velocity.Y *= 4;
+        }
+    }
+}
---
+++ Items/Weapons/Hardmode/BiomeWep/CoolMace.cs
@@ -0,0 +1,106 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Items.Weapons.Hardmode.BiomeWep
+{
+    public class CoolMace : ModItem
+    {
+
+
+        float SwingRange = 0;
+        int Timerset = 0;
+        int TimerReal = 0;
+
+
+        public override void SetDefaults()
+        {
+            item.noMelee = true;
+            item.melee = true;
+            item.damage = 120;
+            item.useTime = 12;
+            item.useAnimation = 12;
+            item.useStyle = 5;
+            item.knockBack = 2;
+            item.noUseGraphic = true;
+            item.rare = ItemRarityID.Yellow;
+            item.crit = 1;
+            item.UseSound = SoundID.Item71;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("MaceArround");
+            item.shootSpeed = 0f;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cool Mace");
+            Tooltip.SetDefault("Swing an Mace arround you \nCharge up range with your left click, then right Click to throw the Mace");
+        }
+
+
+
+        public override bool AltFunctionUse(Player player)//You use this to allow the item to be right clicked
+        {
+            return true;
+        }
+
+
+
+        public override bool CanUseItem(Player player)
+        {
+
+            if(SwingRange > 15) 
+            {
+                SwingRange = 15;
+            }
+
+
+            if(Timerset > 0) 
+            {
+                SwingRange = 0;
+                TimerReal = 0;
+                Timerset = 0;
+            }
+
+            
+            for (int i = 0; i < 1000; ++i)
+            {
+                if (Main.projectile[i].active && Main.projectile[i].owner == Main.myPlayer && Main.projectile[i].type == item.shoot)
+                {
+                    return false;
+                }
+            }
+            if (player.altFunctionUse == 2)//Sets what happens on right click(special ability)
+            {
+                Timerset++;
+                item.shoot = mod.ProjectileType("IceChunk");
+                item.useTime = 22;
+                item.useAnimation = 22;
+                item.shootSpeed = 3f + SwingRange;
+                item.UseSound = SoundID.Item19;
+                item.useStyle = ItemUseStyleID.SwingThrow;
+            }
+            else //Sets what happens on left click(normal use)
+            {
+                SwingRange = SwingRange + 2.8f;
+                item.shoot = mod.ProjectileType("MaceArround");
+                item.useTime = 12;
+                item.useAnimation = 12;
+                item.shootSpeed = 0f;
+                item.UseSound = SoundID.Item71;
+                item.useStyle = 5;
+            }
+
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("FrostEssence"), 17);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
+
