434e43f01735bd82365bb328470b3df128ed3f6d
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-06-21 12:06:15 -0400

    Oops
    - removed something not useful

--- Tiles/HerosAltar.cs
+++ Tiles/HerosAltar.cs
@@ -25,18 +25,6 @@ namespace OvermorrowMod.Tiles
 			name.SetDefault("Hero's Altar");
 			AddMapEntry(new Color(253, 221, 3), name);
 		}
-		public override void HitWire(int i, int j) {
-			Tile tile = Main.tile[i, j];
-			int topY = j - tile.frameY / 18 % 3;
-			short frameAdjustment = (short)(tile.frameX > 0 ? -18 : 18);
-			Main.tile[i, topY].frameX += frameAdjustment;
-			Main.tile[i, topY + 1].frameX += frameAdjustment;
-			Main.tile[i, topY + 2].frameX += frameAdjustment;
-			Wiring.SkipWire(i, topY);
-			Wiring.SkipWire(i, topY + 1);
-			Wiring.SkipWire(i, topY + 2);
-			NetMessage.SendTileSquare(-1, i, topY + 1, 3, TileChangeType.None);
-		}
 		
 		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
 			Item.NewItem(i * 16, j * 16, 32, 16, ModContent.ItemType<Items.Weapons.PreHardmode.Melee.HerosBlade>());
