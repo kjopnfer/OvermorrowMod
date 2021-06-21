ea76526a7f2d546fb7f2b95950277e8993c1c64e
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-06-21 10:53:42 -0400

    Added Holy Shot

---
+++ Items/Weapons/Hardmode/HardMagic/HolyShottet.cs
@@ -0,0 +1,50 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Items.Weapons.Hardmode.HardMagic
+{
+    class HolyShottet : ModItem
+    {        
+        public override void SetDefaults()
+        {
+            item.noMelee = true;
+            item.magic = true;
+            item.damage = 85;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.knockBack = 10;
+            item.rare = ItemRarityID.Blue;
+            item.crit = 0;
+            item.mana = 1;
+            item.UseSound = SoundID.Item34;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("HolyLight");
+            item.shootSpeed = 0f;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Holy Shot");
+            Tooltip.SetDefault("Shoots shots of light in a circle that come to you");
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.HallowedBar, 10);
+
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-2, 0);
+        }
+    }
+}
