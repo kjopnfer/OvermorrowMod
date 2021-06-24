99d5f884dc209da108e63cf0790199b421c36980
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-06-24 15:47:27 -0400

    Better Blood

--- Projectiles/Boss/BloodyBall.cs
+++ Projectiles/Boss/BloodyBall.cs
@@ -30,11 +30,11 @@ namespace OvermorrowMod.Projectiles.Boss
             Lighting.AddLight(projectile.Center, 1f, 0f, 0f);
 
             projectile.ai[0]++;
-
+            Color Bloodc = Color.Red;
             projectile.localAI[0] += 1f;
             if (projectile.localAI[0] > 3f)
             {
-                int num1110 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 5, projectile.velocity.X, projectile.velocity.Y, 50, default(Color), 1.6f);
+                int num1110 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 12, projectile.velocity.X, projectile.velocity.Y, 50, Bloodc, 1.6f);
                 Main.dust[num1110].position = (Main.dust[num1110].position + projectile.Center) / 2f;
                 Main.dust[num1110].noGravity = true;
                 Dust dust81 = Main.dust[num1110];
