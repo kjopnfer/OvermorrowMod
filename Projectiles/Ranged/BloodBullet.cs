92cf1b069241058e517da82c7bc9a85378995a38
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-06-29 12:38:02 -0400

    Blood Gun Added

---
+++ Projectiles/Ranged/BloodBullet.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Projectiles.Ranged
+{
+    public class BloodBullet : ModProjectile
+    {
+
+        public override void SetDefaults()
+        {
+            projectile.width = 2;
+            projectile.height = 20;
+            projectile.penetrate = 1;
+            projectile.friendly = true;
+            projectile.hostile = false;
+        }
+
+		public override void AI() 
+		{
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90f);
+        }
+        
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if(target.life < projectile.damage - target.defense)
+            {
+            Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0, 0, ModContent.ProjectileType<BloodBulletBoom>(), projectile.damage * 2, 1f, projectile.owner, 0f);
+            }
+        }
+    }
+}
---
+++ Projectiles/Ranged/BloodBulletBoom.cs
@@ -0,0 +1,43 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace OvermorrowMod.Projectiles.Ranged
+{
+    public class BloodBulletBoom : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.hostile = false;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.width = 100;
+            projectile.height = 100;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 1; //The amount of time the projectile is alive for
+        }
+
+        public override void Kill(int timeLeft)
+        {
+
+            Vector2 position = projectile.Center;
+            Main.PlaySound(SoundID.Item14, (int)position.X, (int)position.Y);
+            int radius = 20;     //this is the explosion radius, the highter is the value the bigger is the explosion
+
+            for (int x = -radius; x <= radius; x++)
+            {
+                for (int y = -radius; y <= radius; y++)
+                {
+
+                    if (Math.Sqrt(x * x + y * y) <= radius + 0.5)   //this make so the explosion radius is a circle
+                    {
+                        Color color = Color.Red;
+                        Dust.NewDust(position, 5, 5, 162, 0.0f, 0.0f, 120, color, 3.3f);  //this is the dust that will spawn after the explosion
+                    }
+                }
+            }
+        }
+    }
+}
