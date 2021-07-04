afa489e8814fe553f290f830e4b53accfc2cb6c5
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-07-04 13:12:55 -0400

    Shroom Flame thrower

---
+++ Items/Weapons/PreHardmode/Ranged/ShroomThrower.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using OvermorrowMod.Projectiles.Ranged;
+using Microsoft.Xna.Framework;
+
+namespace OvermorrowMod.Items.Weapons.PreHardmode.Ranged
+{
+    public class ShroomThrower : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mushroom Flamethrower");
+            Tooltip.SetDefault("Uses glowing mushrooms as ammo \nHas a chance to inflict fungal infection");
+        }
+        public override void SetDefaults()
+        {
+            item.rare = ItemRarityID.Blue;
+            item.width = 32;
+            item.height = 32;
+            item.damage = 12;
+            item.ranged = true;
+            item.noMelee = true;
+            item.useTime = 10;
+            item.useAnimation = 30;
+            item.UseSound = SoundID.Item34;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.autoReuse = true;
+            item.knockBack = 0.1f;
+            item.shoot = ModContent.ProjectileType<ShroomFlame>();
+            item.useAmmo = 183;
+            item.shootSpeed = 15f;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-2, 0);
+        }
+    }
+}
