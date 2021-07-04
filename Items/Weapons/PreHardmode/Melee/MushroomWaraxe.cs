0b55a9a4675ecb1777e17038fab6a6bdf7ce0770
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-07-03 20:22:08 -0400

    Mushroom Waraxe

---
+++ Items/Weapons/PreHardmode/Melee/MushroomWaraxe.cs
@@ -0,0 +1,51 @@
+using Microsoft.Xna.Framework;
+using OvermorrowMod.Items.Materials;
+using OvermorrowMod.Projectiles.Melee;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Items.Weapons.PreHardmode.Melee
+{
+    public class MushroomWaraxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mushroom Waraxe");
+        }
+
+        public override void SetDefaults()
+        {
+            item.scale = 1.3f;
+            item.rare = ItemRarityID.Blue;
+            item.UseSound = SoundID.Item7;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.damage = 19;
+            item.useTime = 25;
+            item.useAnimation = 25;
+            item.knockBack = 3f;
+            item.melee = true;
+            item.autoReuse = true;
+            item.value = Item.sellPrice(gold: 1);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(183, 35);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+		public override void MeleeEffects(Player player, Rectangle hitbox) 
+        {
+			if (Main.rand.NextBool(10)) 
+            {
+				int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 41);
+			}
+		}
+	}
+}
+
+
