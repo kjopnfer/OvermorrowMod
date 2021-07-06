b3f13fa2d978f920b0436f6cefc09d86da48cebd
Justin Lai <gaypal0805@gmail.com>
2021-07-06 10:13:27 -0400

    Updated Falchion projectile sprite

--- Projectiles/Melee/WaterSlash.cs
+++ Projectiles/Melee/WaterSlash.cs
@@ -11,11 +11,6 @@ namespace OvermorrowMod.Projectiles.Melee
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Water Slash");
-            Main.projFrames[projectile.type] = 4;
-
-            // Afterimage effect
-            /*ProjectileID.Sets.TrailCacheLength[projectile.type] = 10;
-            ProjectileID.Sets.TrailingMode[projectile.type] = 0;*/
         }
 
         public override void SetDefaults()
@@ -35,46 +30,13 @@ namespace OvermorrowMod.Projectiles.Melee
 
         public override void AI()
         {
-            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(180);
+            projectile.rotation = projectile.velocity.ToRotation();
 
             // Make projectiles gradually disappear
             if (projectile.timeLeft <= 20)
             {
                 projectile.alpha += 10;
             }
-
-            // Loop through the 4 animation frames, spending 12 ticks on each.
-            if (++projectile.frameCounter >= 6)
-            {
-                projectile.frameCounter = 0;
-                if (++projectile.frame >= Main.projFrames[projectile.type])
-                {
-                    projectile.frame = 0;
-                }
-            }
-        }
-
-        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
-        {
-            Color color26 = Color.GreenYellow;
-            Texture2D texture2D16 = mod.GetTexture("Projectiles/Melee/WaterSlash");
-
-            int num154 = Main.projectileTexture[projectile.type].Height / Main.projFrames[projectile.type];
-            int y2 = num154 * projectile.frame;
-            Rectangle drawRectangle = new Microsoft.Xna.Framework.Rectangle(0, y2, Main.projectileTexture[projectile.type].Width, num154);
-
-            Vector2 origin2 = drawRectangle.Size() / 2f;
-
-            for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[projectile.type]; i++)
-            {
-                Color color27 = color26;
-                color27 *= (float)(ProjectileID.Sets.TrailCacheLength[projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[projectile.type];
-                Vector2 value4 = projectile.oldPos[i];
-                float num165 = projectile.oldRot[i];
-                Main.spriteBatch.Draw(texture2D16, value4 + projectile.Size / 2f - Main.screenPosition + new Vector2(0, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(drawRectangle), color27, num165, origin2, projectile.scale, SpriteEffects.None, 0f);
-            }
-
-            return base.PreDraw(spriteBatch, lightColor);
         }
 
         public override void Kill(int timeLeft)
