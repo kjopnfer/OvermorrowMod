d131e26d08b823cc7882e8c69f3cfb585987a42e
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-07-12 18:30:35 -0400

    Upgraded Mega Blaster

--- Items/Weapons/PreHardmode/Magic/MegaBlaster.cs
+++ Items/Weapons/PreHardmode/Magic/MegaBlaster.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Xna.Framework;
+﻿
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Localization;
+using static Terraria.ModLoader.ModContent;
 
 namespace OvermorrowMod.Items.Weapons.PreHardmode.Magic
 {
@@ -14,7 +18,7 @@ namespace OvermorrowMod.Items.Weapons.PreHardmode.Magic
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Mega Blaster");
-            Tooltip.SetDefault("Can be charged to make powerfull blast");
+            Tooltip.SetDefault("Can be charged to make powerfull blast \nOnly shoots on the X axis");
         }
         public override void SetDefaults()
         {
@@ -42,7 +46,7 @@ namespace OvermorrowMod.Items.Weapons.PreHardmode.Magic
             {
                 Soundtimer = 0;
             }
-            if(Charge > 10 && Charge < 60)
+            if(Charge < 60)
             {
                 item.shoot = mod.ProjectileType("Blast1");
             }
@@ -100,18 +104,391 @@ namespace OvermorrowMod.Items.Weapons.PreHardmode.Magic
                     }
             }
 
-            if(item.shoot == mod.ProjectileType("Blast2") && Charge < 60)
-            {
-                item.shoot = mod.ProjectileType("Blast1");
-            }
-
-            if(item.shoot == mod.ProjectileType("Blast3") && Charge < 120)
-            {
-                item.shoot = mod.ProjectileType("Blast1");
-            }
-
         }
 
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Charge = 0;
+            return true;
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-3, 0);
+        }
+    }
+
+
+
+    public class EyeBlaster : ModItem
+    {
+
+        int Soundtimer = 0;
+        int Charge = 0;
+        int Mode = 0;
+        int eye = 0;
+        int normal = 0;
+        public override string Texture => "OvermorrowMod/Items/Weapons/PreHardmode/Magic/MegaBlaster";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mega Blaster V1");
+            Tooltip.SetDefault("Can be charged to make powerfull blast \nOnly shoots on the X axis \nUse rightclick to change modes and leftclick to fire mode projectile \nUpgrade descriptions: Eye Blast - Shoot out a penetrating projectile that gains damage when hitting an enemy");
+        }
+        public override void SetDefaults()
+        {
+            item.rare = ItemRarityID.Blue;
+            item.width = 32;
+            item.height = 32;
+            item.damage = 17;
+            item.magic = true;
+            item.UseSound = SoundID.Item12;
+            item.noMelee = true;
+            item.useTime = 13;
+            item.useAnimation = 13;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.autoReuse = true;
+            item.knockBack = 0.5f;
+            item.shoot = mod.ProjectileType("Blast1");
+            item.shootSpeed = 0f;
+        }
+
+
+
+        private void BossText(string text) // boss messages
+        {
+            if (Main.netMode == NetmodeID.SinglePlayer)
+            { 
+                Main.NewText(text, Color.Blue);
+            }
+            else if (Main.netMode == NetmodeID.Server)
+            {
+                NetMessage.BroadcastChatMessage(NetworkText.FromLiteral(text), Color.Blue);
+            }
+        }
+
+
+        public override bool AltFunctionUse(Player player)//You use this to allow the item to be right clicked
+        {
+            return true;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if(Mode == 0)
+            {
+                item.mana = 0;
+                normal++;
+                eye = 0;
+                Charge++;
+                Soundtimer++;
+
+                if(Soundtimer > 10)
+                {
+                    Soundtimer = 0;
+                }
+                if(Charge < 60)
+                {
+                    item.shoot = mod.ProjectileType("Blast1");
+                }
+                if(Charge > 59 && Charge < 120)
+                {
+                    if(Soundtimer == 2)
+                    {
+                        Main.PlaySound(SoundID.Item46, player.Center);
+                    }
+                    item.shoot = mod.ProjectileType("Blast2");
+
+                    int Random1 = Main.rand.Next(-57, 57);
+                    int Random2 = Main.rand.Next(-57, 57);
+
+                    float XDustposition = player.Center.X + Random1 - player.width / 2;
+                    float YDustposition = player.Center.Y + Random2 - player.height / 2;
+                    Vector2 VDustposition = new Vector2(XDustposition, YDustposition);
+                    Vector2 Dusttarget = player.Center - new Vector2(player.width / 2, player.height / 2);
+                    Vector2 Dustdirection = Dusttarget - VDustposition;
+                    Dustdirection.Normalize();
+
+                    Color granitedustc = Color.Yellow;
+                    {
+                        int dust = Dust.NewDust(VDustposition, player.width, player.height, 16, 0.0f, 0.0f, 10, granitedustc, 1.8f);
+                        Main.dust[dust].noGravity = true;
+                        Vector2 velocity = Dustdirection * 2f;
+                        Main.dust[dust].velocity = Dustdirection * 2f;
+                    }
+                }
+
+                if(Charge > 119)
+                {
+                    if(Soundtimer == 2)
+                    {
+                        Main.PlaySound(SoundID.Item20, player.Center);
+                    }
+                        item.shoot = mod.ProjectileType("Blast3");
+
+                        int Random1 = Main.rand.Next(-57, 57);
+                        int Random2 = Main.rand.Next(-57, 57);
+
+                        float XDustposition = player.Center.X + Random1 - player.width / 2;
+                        float YDustposition = player.Center.Y + Random2 - player.height / 2;
+                        Vector2 VDustposition = new Vector2(XDustposition, YDustposition);
+                        Vector2 Dusttarget = player.Center - new Vector2(player.width / 2, player.height / 2);
+                        Vector2 Dustdirection = Dusttarget - VDustposition;
+                        Dustdirection.Normalize();
+
+                        Color granitedustc = Color.Cyan;
+                        {
+                            int dust = Dust.NewDust(VDustposition, player.width, player.height, 16, 0.0f, 0.0f, 10, granitedustc, 1.8f);
+                            Main.dust[dust].noGravity = true;
+                            Vector2 velocity = Dustdirection * 2f;
+                            Main.dust[dust].velocity = Dustdirection * 2f;
+                        }
+                    }
+                
+            }
+
+            if(Mode == 1)
+            {
+                item.color = Color.Red;
+                eye++;
+                normal = 0;
+                Charge = 0;
+                item.shoot = mod.ProjectileType("EyeBlast");
+                item.mana = 11;
+            }
+
+            if(Mode == 0)
+            {
+                item.color = new Color();
+            }
+
+            if(eye == 1)
+            {
+                BossText("Eye Blast");
+            }
+            
+            if(normal == 1)
+            {
+                BossText("Normal Shot");
+            }
+
+            if(Mode == 2)
+            {
+                Mode = 0;
+            }
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)//Sets what happens on right click(special ability)
+            {
+                Mode += 1;
+            }
+            return true;
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Charge = 0;
+            return true;
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-3, 0);
+        }
+    }
+
+
+
+
+    public class EvilBlaster : ModItem
+    {
+
+        int Soundtimer = 0;
+        int Charge = 0;
+        int Mode = 0;
+        int eye = 0;
+        int evil = 0;
+        int normal = 0;
+        public override string Texture => "OvermorrowMod/Items/Weapons/PreHardmode/Magic/MegaBlaster";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mega Blaster V2");
+            Tooltip.SetDefault("Can be charged to make powerfull blast \nOnly shoots on the X axis \nUse rightclick to change modes and leftclick to fire mode projectile \nUpgrade descriptions: Eye Blast - Shoot out a penetrating projectile that gains damage when hitting an enemy \n Vile Shotgun - Shoots a spliting, bouncing ball of evil");
+        }
+        public override void SetDefaults()
+        {
+            item.rare = ItemRarityID.Green;
+            item.width = 32;
+            item.height = 32;
+            item.damage = 20;
+            item.magic = true;
+            item.UseSound = SoundID.Item12;
+            item.noMelee = true;
+            item.useTime = 13;
+            item.useAnimation = 13;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.autoReuse = true;
+            item.knockBack = 0.5f;
+            item.shoot = mod.ProjectileType("Blast1");
+            item.shootSpeed = 0f;
+        }
+
+
+
+        private void BossText(string text) // boss messages
+        {
+            if (Main.netMode == NetmodeID.SinglePlayer)
+            { 
+                Main.NewText(text, Color.Blue);
+            }
+            else if (Main.netMode == NetmodeID.Server)
+            {
+                NetMessage.BroadcastChatMessage(NetworkText.FromLiteral(text), Color.Blue);
+            }
+        }
+
+
+        public override bool AltFunctionUse(Player player)//You use this to allow the item to be right clicked
+        {
+            return true;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if(Mode == 0)
+            {
+                item.mana = 0;
+                normal++;
+                evil = 0;
+                eye = 0;
+                Charge++;
+                Soundtimer++;
+
+                if(Soundtimer > 10)
+                {
+                    Soundtimer = 0;
+                }
+                if(Charge < 60)
+                {
+                    item.shoot = mod.ProjectileType("Blast1");
+                }
+                if(Charge > 59 && Charge < 120)
+                {
+                    if(Soundtimer == 2)
+                    {
+                        Main.PlaySound(SoundID.Item46, player.Center);
+                    }
+                    item.shoot = mod.ProjectileType("Blast2");
+
+                    int Random1 = Main.rand.Next(-57, 57);
+                    int Random2 = Main.rand.Next(-57, 57);
+
+                    float XDustposition = player.Center.X + Random1 - player.width / 2;
+                    float YDustposition = player.Center.Y + Random2 - player.height / 2;
+                    Vector2 VDustposition = new Vector2(XDustposition, YDustposition);
+                    Vector2 Dusttarget = player.Center - new Vector2(player.width / 2, player.height / 2);
+                    Vector2 Dustdirection = Dusttarget - VDustposition;
+                    Dustdirection.Normalize();
+
+                    Color granitedustc = Color.Yellow;
+                    {
+                        int dust = Dust.NewDust(VDustposition, player.width, player.height, 16, 0.0f, 0.0f, 10, granitedustc, 1.8f);
+                        Main.dust[dust].noGravity = true;
+                        Vector2 velocity = Dustdirection * 2f;
+                        Main.dust[dust].velocity = Dustdirection * 2f;
+                    }
+                }
+
+                if(Charge > 119)
+                {
+                    if(Soundtimer == 2)
+                    {
+                        Main.PlaySound(SoundID.Item20, player.Center);
+                    }
+                        item.shoot = mod.ProjectileType("Blast3");
+
+                        int Random1 = Main.rand.Next(-57, 57);
+                        int Random2 = Main.rand.Next(-57, 57);
+
+                        float XDustposition = player.Center.X + Random1 - player.width / 2;
+                        float YDustposition = player.Center.Y + Random2 - player.height / 2;
+                        Vector2 VDustposition = new Vector2(XDustposition, YDustposition);
+                        Vector2 Dusttarget = player.Center - new Vector2(player.width / 2, player.height / 2);
+                        Vector2 Dustdirection = Dusttarget - VDustposition;
+                        Dustdirection.Normalize();
+
+                        Color granitedustc = Color.Cyan;
+                        {
+                            int dust = Dust.NewDust(VDustposition, player.width, player.height, 16, 0.0f, 0.0f, 10, granitedustc, 1.8f);
+                            Main.dust[dust].noGravity = true;
+                            Vector2 velocity = Dustdirection * 2f;
+                            Main.dust[dust].velocity = Dustdirection * 2f;
+                        }
+                    }
+                
+            }
+
+            if(Mode == 1)
+            {
+                item.color = Color.Red;
+                eye++;
+                normal = 0;
+                Charge = 0;
+                item.shoot = mod.ProjectileType("EyeBlast");
+                item.mana = 9;
+            }
+
+
+            if(Mode == 2)
+            {
+                item.color = Color.Purple;
+                evil++;
+                normal = 0;
+                Charge = 0;
+                item.shoot = mod.ProjectileType("EvilShotBlast1");
+                item.mana = 11;
+            }
+
+
+
+
+            if(Mode == 0)
+            {
+                item.color = new Color();
+            }
+
+            if(eye == 1)
+            {
+                BossText("Eye Blast");
+            }
+            
+            if(normal == 1)
+            {
+                BossText("Normal Shot");
+            }
+
+            if(evil == 1)
+            {
+                BossText("Vile Shotgun");
+            }
+
+
+
+
+            if(Mode == 3)
+            {
+                Mode = 0;
+            }
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)//Sets what happens on right click(special ability)
+            {
+                Mode += 1;
+            }
+            return true;
+        }
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
             Charge = 0;
