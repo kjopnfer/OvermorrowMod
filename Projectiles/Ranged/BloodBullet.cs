6eacab86a0f646cafd45c8315f3f9188863f2933
TPLUCodeManipulationUnit <74583338+TPLUCodeManipulationUnit@users.noreply.github.com>
2021-07-13 14:48:08 -0600

    Hemanemesis is good now

--- Projectiles/Ranged/BloodBullet.cs
+++ Projectiles/Ranged/BloodBullet.cs
@@ -12,48 +12,35 @@ namespace OvermorrowMod.Projectiles.Ranged
 {
     public class BloodBullet : ModProjectile
     {
-        private bool hit = false;
-        private Vector2 velocitystash = Vector2.Zero;
         public override void SetDefaults()
         {
             projectile.width = 2;
             projectile.height = 20;
             projectile.penetrate = -1;//2;//1;
-            projectile.timeLeft = 600;
+            projectile.timeLeft = 240;
             projectile.friendly = true;
             projectile.hostile = false;
+            projectile.alpha = 255;
         }
-
         public override void AI() 
 		{
-            projectile.velocity = projectile.velocity * 1.0001f;
-            if (hit)
+            Lighting.AddLight(projectile.Center, 1f, 0f, 0f);
+            projectile.damage = 17;
+            projectile.ai[0]++;
+            Color Bloodc = Color.Red;
+            projectile.localAI[0] += 1f;
+            if (projectile.localAI[0] > 3f)
             {
-                projectile.damage = 0;
-                velocitystash = Vector2.SmoothStep(velocitystash, Vector2.Zero, 0.1f);
-                projectile.velocity = Vector2.Zero;
-                projectile.ai[1] -= 0.01f;
-                if (projectile.ai[1] <= 0)
-                {
-                    projectile.Kill();
-                }
-
-            }
-            else
-            {
-                projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90f);
-                projectile.ai[1] = 1;
-                Vector2.SmoothStep(projectile.velocity, Vector2.Zero, 0.01f);
-                velocitystash = projectile.velocity;
-                if (projectile.timeLeft < 240)
-                {
-                    hit = true;
-                }
+                int num1110 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 12, projectile.velocity.X, projectile.velocity.Y, 50, Bloodc, 1.6f);
+                Main.dust[num1110].position = (Main.dust[num1110].position + projectile.Center) / 2f;
+                Main.dust[num1110].noGravity = true;
+                Dust dust81 = Main.dust[num1110];
+                dust81.velocity *= 0.5f;
             }
         }
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-            if(target.life < projectile.damage - (target.defense * 1.1))
+            if (target.life < projectile.damage - (target.defense * 1.1))
             {
                 for (int i = 0; i < Main.rand.Next(3, 5); i++)
                 {
@@ -63,38 +50,6 @@ namespace OvermorrowMod.Projectiles.Ranged
                     }
                 }
             }
-            hit = true;
-        }
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            hit = true;
-            return false;
-        }
-        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
-        {
-            PrimitivePacket packet = new PrimitivePacket();
-            packet.Type = PrimitiveType.TriangleStrip;
-            float range = Math.Abs(velocitystash.X) + Math.Abs(velocitystash.Y) + 6;
-            Vector2 start = projectile.Center;
-            Vector2 end = projectile.Center + ((-1 * velocitystash) * range);
-            float baseSize = 6;
-            for (float i = 0; i < 99; i += /*0.25f*/ /*0.1f*/ 0.125f)
-            {
-                float progress1 = (float)i / (float)100;
-                Vector2 pos1 = Vector2.Lerp(start, end, progress1);
-                Vector2 offset1 = ((Vector2.UnitX * projectile.ai[1]) * (baseSize * (1 - progress1))).RotatedBy(projectile.rotation);
-                Vector2 offset2 = ((Vector2.UnitX * projectile.ai[1]) * (-baseSize * (1 - progress1))).RotatedBy(projectile.rotation);
-                Vector2 offset3 = ((Vector2.UnitX * projectile.ai[1]) * (baseSize * (1 - progress1))).RotatedBy(projectile.rotation).RotatedBy(MathHelper.ToRadians(270));
-                Color color1 = Color.Lerp(Color.DarkRed, Color.MediumVioletRed, progress1) * progress1;
-
-                packet.Add(pos1 + offset1, color1, new Vector2(progress1 + Main.GlobalTime, 1));
-                packet.Add(pos1 + offset2, color1, new Vector2(progress1 + Main.GlobalTime, 0));
-                packet.Add(pos1 + offset3, color1, new Vector2(progress1 + Main.GlobalTime, 0.5f));
-            }
-            PrimitivePacket.SetTexture(0, Main.projectileTexture[projectile.type]);
-            packet.Pass = "Texturized"; //"Basic";
-            packet.Send();
-            return false;
         }
     }
 }
