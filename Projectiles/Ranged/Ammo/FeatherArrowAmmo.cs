42b391738c40924d9939f58819ca9ab3b384e9ca
Rose658 <81722588+Rose658@users.noreply.github.com>
2021-07-18 19:34:37 -0400

    New Ammos

---
+++ Projectiles/Ranged/Ammo/FeatherArrowAmmo.cs
@@ -0,0 +1,40 @@
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Projectiles.Ranged.Ammo
+{
+	public class FeatherArrowAmmo : ModItem
+	{
+		public override void SetStaticDefaults() 
+        {
+            DisplayName.SetDefault("Featherfall Arrow");
+			Tooltip.SetDefault("Arrow that falls slower then normal");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 8;
+			item.ranged = true;
+			item.width = 8;
+			item.height = 8;
+			item.maxStack = 999;
+			item.consumable = true;             //You need to set the item consumable so that the ammo would automatically consumed
+			item.knockBack = 1.5f;
+			item.value = 10;
+			item.rare = ItemRarityID.Green;
+			item.shoot = ModContent.ProjectileType<FeatherArrow>();   //The projectile shoot when your weapon using this ammo
+			item.shootSpeed = 8f;                  //The speed of the projectile
+			item.ammo = AmmoID.Arrow;              //The ammo class this ammo belongs to.
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.WoodenArrow, 50);
+			recipe.AddIngredient(ItemID.Feather, 1);
+			recipe.SetResult(this, 50);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Projectiles/Ranged/Ammo/FungalArrow.cs
@@ -0,0 +1,58 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using OvermorrowMod.Buffs.Debuffs;
+
+namespace OvermorrowMod.Projectiles.Ranged.Ammo
+{
+    public class FungalArrow : ModProjectile
+    {
+
+        public override void SetDefaults()
+        {
+            projectile.width = 34;
+            projectile.height = 34;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 600;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fungal Arrow");
+        }
+
+		public override void AI() 
+		{
+            projectile.velocity.Y += 0.07f;
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90f);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(3) == 2)
+            {
+                target.AddBuff(ModContent.BuffType<FungalInfection>(), 280);
+            }
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            if (Main.rand.Next(5) == 3)
+            {
+                Item.NewItem((int)projectile.position.X, (int)projectile.position.Y, projectile.width, projectile.height, ModContent.ItemType<FungalArrowAmmo>());
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            // Makes dust projectiled on tile
+            Collision.HitTiles(projectile.position, projectile.velocity, projectile.width, projectile.height);
+            Main.PlaySound(SoundID.Item10, projectile.position);
+            return true;
+        }
+    }
+}
