5e1c77d6182391d5e01e6f13de2c6be63b637bcb
Justin Lai <gaypal0805@gmail.com>
2021-07-25 22:08:03 -0400

    Stop breaking oh my god

--- OvermorrowWorld.cs
+++ OvermorrowWorld.cs
@@ -443,7 +443,7 @@ namespace OvermorrowMod
                 WorldGen.PlaceTile(x + 22, y - 6, 311);
                 WorldGen.PlaceTile(x + 23, y - 6, 311);
                 WorldGen.PlaceTile(x + 23, y - 7, 311);
-                WorldGen.PlaceTile(x + 24, y - 7, 311);  
+                WorldGen.PlaceTile(x + 24, y - 7, 311);
 
 
                 //back
@@ -522,15 +522,6 @@ namespace OvermorrowMod
                     WorldGen.PlaceWall(x + i + 1, y - 1, 42);
                 }
             }
-
-
-
-
-
-
-
-
-
 
             for (int k = 0; k < (int)((Main.maxTilesX * Main.maxTilesY) * 0.0000008); k++)
             {
@@ -641,7 +632,7 @@ namespace OvermorrowMod
                 }
 
                 WorldGen.PlaceTile(x - 12, y - 7, 10);
-                
+
 
 
 
@@ -787,10 +778,7 @@ namespace OvermorrowMod
 
                 WorldGen.PlaceTile(x - 6, y - 7, 21, style: 28);
             }
-
-
-        
-
+        }
 
         bool activetilecheck = false;
         bool TowerPlaced = false;
