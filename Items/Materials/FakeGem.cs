16b977b9f0f5f8d21d822a9a9180ddda6293409a
kjopnfer <gaypal0805@gmail.com>
2021-09-04 22:42:26 -0400

    Added Text Shader

--- Items/Materials/FakeGem.cs
+++ Items/Materials/FakeGem.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using OvermorrowMod.Tiles.Block;
 using Terraria;
 using Terraria.ID;
@@ -26,7 +27,32 @@ namespace OvermorrowMod.Items.Materials
             item.useTime = 10;
             item.useStyle = ItemUseStyleID.SwingThrow;
         }
-        
+
+        public override bool PreDrawTooltipLine(DrawableTooltipLine line, ref int yOffset)
+        {
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            if (line.Name == "ItemName")
+            {
+                spriteBatch.Reload(SpriteSortMode.Immediate);
+                Effect fx = OvermorrowModFile.Mod.TextShader;
+                Main.graphics.GraphicsDevice.Textures[0] = mod.GetTexture("Textures/Perlin");
+                fx.Parameters["uColor0"].SetValue(Color.Red.ToVector3());
+                //fx.Parameters["uColor1"].SetValue(Color.Black.ToVector3());
+                //fx.Parameters["uColor0"].SetValue(new Color(140, 48, 85).ToVector3());
+                //fx.Parameters["uColor1"].SetValue(new Color(176, 48, 62).ToVector3());
+                fx.Parameters["uColor1"].SetValue(Color.Purple.ToVector3());
+                fx.SafeSetParameter("uTime", Main.GlobalTime);
+                //fx.CurrentTechnique.Passes["Noise"].Apply();
+                fx.CurrentTechnique.Passes[0].Apply();
+                Utils.DrawBorderString(spriteBatch, line.text, new Vector2(line.X, line.Y), Color.Lerp(Color.Red, Color.Transparent, 0.95f), 1f, Main.rand.NextFloat(-0.3f, 0.3f), Main.rand.NextFloat(-0.2f, 0.2f));
+                Utils.DrawBorderString(spriteBatch, line.text, new Vector2(line.X, line.Y), Color.Lerp(Color.Orange, Color.Transparent, 0.95f), 1f, Main.rand.NextFloat(-0.1f, 0.1f), Main.rand.NextFloat(-0.4f, 0.4f));
+                Utils.DrawBorderString(spriteBatch, line.text, new Vector2(line.X, line.Y), Color.White, 1f);
+                spriteBatch.Reload(SpriteSortMode.Deferred);
+                return false;
+            }
+            return true;
+        }
+
         public override void PostUpdate()
         {
             Lighting.AddLight(item.Center, 1f, 0f, 0f);
