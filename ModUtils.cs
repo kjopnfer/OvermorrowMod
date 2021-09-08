1a7befb67525780db6493017c4ed6587393590d5
Wooodio <70658958+Wooodio@users.noreply.github.com>
2021-09-08 12:33:01 -0300

    Added player.Overmorrow()
    just a simplified version of player.GetModPlayer<OvermorrowPlayer>()

--- ModUtils.cs
+++ ModUtils.cs
@@ -34,51 +34,11 @@ namespace OvermorrowMod
         {
             stopRain.Invoke(null, null);
         }
-
-        public static void Draw(this Rectangle rect, bool collides = false)
-        {
-            PrimitivePacket packet = new PrimitivePacket();
-            packet.Type = PrimitiveType.TriangleList;
-            packet.Pass = "Basic";
-            Vector2 pos1 = rect.TopLeft();
-            Vector2 pos2 = rect.BottomLeft();
-            Vector2 pos3 = rect.TopRight();
-            Vector2 pos4 = rect.BottomRight();
-            Color color = collides ? Color.Green : Color.Red;
-            packet.Add(pos1, color, Vector2.Zero);
-            packet.Add(pos2, color, Vector2.Zero);
-            packet.Add(pos3, color, Vector2.Zero);
-            packet.Add(pos4, color, Vector2.Zero);
-            packet.Add(pos3, color, Vector2.Zero);
-            packet.Add(pos2, color, Vector2.Zero);
-            packet.Send();
-        }
         public static Vector2 NearestPoint(this Vector2 vec, Rectangle rect)
         {
             float nearX = vec.X > rect.Right ? rect.Right : vec.X < rect.Left ? rect.Left : vec.X;
             float nearY = vec.Y > rect.Bottom ? rect.Bottom : vec.Y < rect.Top ? rect.Top : vec.Y;
             return new Vector2(nearX, nearY);
-        }
-        public static bool CollisionCheck(Rectangle rect1, Rectangle rect2)
-        {
-            Vector2 nearest1 = NearestPoint(rect1.Center.ToVector2(), rect2);
-            Vector2 nearest2 = NearestPoint(rect2.Center.ToVector2(), rect1);
-            return rect1.Contains(nearest1.ToPoint()) || rect2.Contains(nearest2.ToPoint());;
-        }
-        public static Vector2 GetRequiredVelocity(Vector2 from, Vector2 to, float rps, float atr = (float)Math.PI)
-        {
-            float mult = (float)atr / rps;
-            mult /= 10;
-            mult *= 2;
-            float aboutLength = mult * (float)atr;
-            float length = (to - from).Length();
-            float startRot = (to - from).ToRotation();
-            return new Vector2(0, length / aboutLength).RotatedBy(startRot - atr);
-        }
-        public static Vector2 GetRequiredVelocity2(Vector2 from, Vector2 to, float time, float atr = (float)Math.PI)
-        {
-            float timeToRot = atr / time;
-            return GetRequiredVelocity(from, to, timeToRot, atr);
         }
         private static float Bezier(float x1, float x2, float x3, float x4, float t)
         {
@@ -101,12 +61,12 @@ namespace OvermorrowMod
             return new Vector3(vec.X, vec.Y, 0);
         }
         public static void ClampDistance(ref Vector2 start, ref Vector2 end, float maxDistance)
+	{
+		if (Vector2.Distance(end, start) > maxDistance)
 		{
-			if (Vector2.Distance(end, start) > maxDistance)
-			{
-				end = start + Vector2.Normalize(end - start) * maxDistance;
-			}
+			end = start + Vector2.Normalize(end - start) * maxDistance;
 		}
+	}
 		/// <summary> Adjusts a velocity to a gravity </summary>
         public static Vector2 AdjustToGravity(this Vector2 velocity, float gravity, float time)
         {
@@ -123,6 +83,10 @@ namespace OvermorrowMod
             }
             return false;
         }
+	public static OvermorrowModPlayer Overmorrow(this Player player)
+	{
+	    return player.GetModPlayer<OvermorrowModPlayer>();
+	}
 
         public static void SafeSetParameter(this Effect effect, string name, float value)
         {
@@ -163,16 +127,16 @@ namespace OvermorrowMod
         public static void Reload(this SpriteBatch spriteBatch, BlendState blendState = null, SpriteSortMode sortMode = default)
         {
             if ((bool)spriteBatch.GetType().GetField("inBeginEndPair", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(spriteBatch))
-			{
-				spriteBatch.End();
-			}
-			if (blendState == null) blendState = (BlendState)spriteBatch.GetField("blendState");
-			SamplerState state = (SamplerState)spriteBatch.GetField("samplerState");
-			DepthStencilState state2 = (DepthStencilState)spriteBatch.GetField("depthStencilState");
-			RasterizerState state3 = (RasterizerState)spriteBatch.GetField("rasterizerState");
-			Effect effect = (Effect)spriteBatch.GetField("customEffect");
-			Matrix matrix = (Matrix)spriteBatch.GetField("transformMatrix");
-			spriteBatch.Begin(sortMode, blendState, state, state2, state3, effect, matrix);
+	    {
+		spriteBatch.End();
+	    }
+	    if (blendState == null) blendState = (BlendState)spriteBatch.GetField("blendState");
+	    SamplerState state = (SamplerState)spriteBatch.GetField("samplerState");
+	    DepthStencilState state2 = (DepthStencilState)spriteBatch.GetField("depthStencilState");
+	    RasterizerState state3 = (RasterizerState)spriteBatch.GetField("rasterizerState");
+	    Effect effect = (Effect)spriteBatch.GetField("customEffect");
+	    Matrix matrix = (Matrix)spriteBatch.GetField("transformMatrix");
+	    spriteBatch.Begin(sortMode, blendState, state, state2, state3, effect, matrix);
         }
         public static void Reload(this SpriteBatch spriteBatch, BlendState blendState = null)
         {
@@ -193,21 +157,6 @@ namespace OvermorrowMod
         {
             return (bool)spriteBatch.GetType().GetField("inBeginEndPair", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(spriteBatch);
         }
-        /*public static void Reload(this SpriteBatch spriteBatch, BlendState blendState = null)
-        {
-            if ((bool)spriteBatch.GetType().GetField("inBeginEndPair", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(spriteBatch))
-			{
-				spriteBatch.End();
-			}
-			SpriteSortMode sortMode = SpriteSortMode.Deferred;
-			if (blendState == null) blendState = (BlendState)spriteBatch.GetField("blendState");
-			SamplerState state = (SamplerState)spriteBatch.GetField("samplerState");
-			DepthStencilState state2 = (DepthStencilState)spriteBatch.GetField("depthStencilState");
-			RasterizerState state3 = (RasterizerState)spriteBatch.GetField("rasterizerState");
-			Effect effect = (Effect)spriteBatch.GetField("customEffect");
-			Matrix matrix = (Matrix)spriteBatch.GetField("transformMatrix");
-			spriteBatch.Begin(sortMode, blendState, state, state2, state3, effect, matrix);
-        }*/
         public static object GetField(this object obj, string name)
         {
             return obj.GetType().GetField(name, BindingFlags.Instance | BindingFlags.NonPublic).GetValue(obj);
@@ -241,11 +190,6 @@ namespace OvermorrowMod
         {
             Vector2 vel = npc.DirectionTo(pos) * speed;
             npc.velocity = (npc.velocity * divider + vel) / (divider + 1);
-        }
-        public static void DirectMove(this NPC npc, Vector2 pos, float speed, float time, float mult)
-        {
-            Vector2 vel = (pos - npc.Center) / time;
-            npc.velocity = vel * mult;
         }
     }
 }
