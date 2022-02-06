c16636992b8c3d61fbadcfd630d28969fa9dd19b
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-02-06 14:43:56 -0500

    Fixed tiny error

--- Projectiles/Melee/YourMomIsHotter.cs
+++ Projectiles/Melee/YourMomIsHotter.cs
@@ -4,7 +4,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Overmorrow.Projectiles.Melee
+namespace OvermorrowMod.Projectiles.Melee
 {
     class YourMomIsHotter : ModProjectile
     {
