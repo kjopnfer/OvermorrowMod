11247463e0f571ca9680315385f54ba186956776
kinzoku1 <xshadowgamer01@gmail.com>
2022-02-18 22:05:53 -0600

    Dev pet added, buff needs sprite

---
+++ Items/Dev/LokiPet.cs
@@ -0,0 +1,43 @@
+using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace OvermorrowMod.Items.Dev
+{
+    public class LokiPet : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Loki");
+            
+            Main.projPet[projectile.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 42;
+            projectile.height = 42;
+            projectile.CloneDefaults(ProjectileID.BlackCat);
+
+            aiType = ProjectileID.BlackCat;
+        }
+
+        public override bool PreAI()
+        {
+            Player player = Main.player[projectile.owner];
+            return true;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+
+            if (!player.dead && player.HasBuff(ModContent.BuffType<LokiPetBuff>()))
+            {
+                projectile.timeLeft = 2;
+            }
+        }
+    }
+}
