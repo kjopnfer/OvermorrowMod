0c48cbcf4e15b52e5b9f919ffa252455ad87653f
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-03-01 19:32:23 -0500

    The Drippler buff now has drip
    E
    why me no commit main

--- Buffs/Summon/DripplerBuff.cs
+++ Buffs/Summon/DripplerBuff.cs
@@ -16,7 +16,7 @@ namespace OvermorrowMod.Buffs.Summon
 
         public override void Update(Player player, ref int buffIndex)
         {
-            if (player.ownedProjectileCounts[ModContent.ProjectileType<StormWhelp>()] > 0)
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<DripplerFriendly>()] > 0)
             {
                 player.buffTime[buffIndex] = 18000;
             }
