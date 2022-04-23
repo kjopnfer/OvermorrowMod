9a64e24c35fb10fcb95152c83e7ff9ae3066c638
Einar Omang <einar@omang.com>
2022-04-23 08:24:53 +0200

    Improve comments on IL a bit

--- Common/ILEdits.cs
+++ Common/ILEdits.cs
@@ -74,23 +74,32 @@ namespace OvermorrowMod.Common
             c.TryGotoNext(i => i.MatchCall<Tile>("nactive"));
             c.Index--;
 
+            // This first section essentially adds HooksNPC(this) || ... to the start of the condition for checking whether
+            // a tile is grappleable.
             // Inject some code that skips the condition checks for whether to hook onto a tile.
             var label = il.DefineLabel();
+            // Load "this" on the stack.
             c.Emit(OpCodes.Ldarg_0);
+            // Pop "this", then push the result of HooksNPC to the stack
             c.EmitDelegate(HooksNPC);
+            // If the result is "true", goto label
             c.Emit(OpCodes.Brtrue, label);
-
             // Matches a later part of the if (Main.player[this.owner].grapCount < 10) line
             c.TryGotoNext(i => i.MatchLdfld<Player>("grapCount"));
             // Step back so that we are just inside the condition
             c.Index -= 4;
+            // Set a label here.
             c.MarkLabel(label);
 
+            // This bit lets us modify the value of the "flag" variable, and also modify "this" a bit each tick when ai[0] is 2 (attached)
             // Skip to last check of AI_007_GrapplingHooks_CanTileBeLatchedOnTo
             c.TryGotoNext(i => i.MatchCall<Projectile>("AI_007_GrapplingHooks_CanTileBeLatchedOnTo"));
             // Jump down a few instructions to get right after loading flag onto the stack
             c.Index += 5;
+            // Load "this" on the stack
             c.Emit(OpCodes.Ldarg_0);
+            // Pop "this", then push the result of ShouldReleaseHook. The previous instruction loads "flag" on the stack, and the next
+            // jumps if it is false, so this effectively lets us replace "if (flag) {" with "if (ShouldReleaseHook(flag, this)) {"
             c.EmitDelegate(ShouldReleaseHook);
 
             // Should match int num14 = 0
@@ -109,6 +118,12 @@ namespace OvermorrowMod.Common
             c.Emit(OpCodes.Ldarg_0);
             c.EmitDelegate<UngrappleDelegate>(EmitUngrappleDelegate); */
         }
+        /// <summary>
+        /// Return true from this method to override the check for grappling,
+        /// return false for vanilla behavior.
+        /// </summary>
+        /// <param name="proj">Projectile to test</param>
+        /// <returns>True if we should attach the grapple, independent of vanilla rules.</returns>
         private static bool HooksNPC(Projectile proj)
         {
             for (int i = 0; i < Main.maxNPCs; i++)
@@ -132,6 +147,13 @@ namespace OvermorrowMod.Common
             return false;
         }
 
+        /// <summary>
+        /// Return true from this method to release the hook, false to not release it.
+        /// To not make a decision one way or another, return flag.
+        /// </summary>
+        /// <param name="flag">Current status of whether or not to test.</param>
+        /// <param name="proj">Projectile to test for</param>
+        /// <returns>True if we should release the grapple, false to keep it attached.</returns>
         private static bool ShouldReleaseHook(bool flag, Projectile proj)
         {
             if (proj.ai[1] >= 0 && proj.ai[1] < Main.maxNPCs)
