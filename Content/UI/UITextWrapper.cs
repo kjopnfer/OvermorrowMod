a74fa0408df52e35636438b1c1d549fd509ed43c
kjopnfer <gaypal0805@gmail.com>
2022-05-01 01:37:25 -0400

    Fixed object reference error

---
+++ Content/UI/UITextWrapper.cs
@@ -0,0 +1,64 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using OvermorrowMod.Core;
+using ReLogic.Graphics;
+using System;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.GameInput;
+using Terraria.UI;
+using Terraria.UI.Chat;
+
+namespace OvermorrowMod.Content.UI
+{
+    internal class UITextWrapper : UIElement
+    {
+        public string text;
+        //private TextSnippet[] textArray;
+        private string[] textArray = null;
+        private bool wrapText;
+        private int maxWidth;
+        private int maxLines;
+        public UITextWrapper(string text, int maxWidth, int maxLines = 10, bool wrapText = true)
+        {
+            this.text = text;
+            this.maxWidth = maxWidth;
+            this.maxLines = maxLines;
+            this.wrapText = wrapText;
+
+            UpdateText();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            UpdateText();
+        }
+
+        public void UpdateText()
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                textArray = null;
+                return;
+            }
+
+            textArray = Utils.WordwrapString(text, FontAssets.MouseText.Value, maxWidth, maxLines, out _);
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            if (textArray == null) return;
+
+            Vector2 pos = GetDimensions().ToRectangle().TopLeft();
+
+            int offsetY = 0;
+            foreach (var line in textArray)
+            {
+                if (string.IsNullOrEmpty(line)) continue;
+
+                Utils.DrawBorderString(spriteBatch, line, pos + Vector2.UnitY * offsetY, Color.Red);
+                offsetY += 15;
+            }
+        }
+    }
+}
