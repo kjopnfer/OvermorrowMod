f48758487f58de087cd0fc7b6fceb7744f83ab8e
kjopnfer <gaypal0805@gmail.com>
2022-07-23 00:01:30 -0400

    Slightly better miniservant pathing

--- Content/NPCs/Bosses/Eye/MiniServant.cs
+++ Content/NPCs/Bosses/Eye/MiniServant.cs
@@ -16,6 +16,7 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
 {
     public class MiniServant : ModNPC, ITrailEntity
     {
+        private float randomAmplitude;
         private float rotateDirection = 1;
         private float moveSpeed;
         private float turnResistance;
@@ -44,10 +45,11 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
         public ref float AICounter => ref NPC.ai[1];
         public override void OnSpawn(IEntitySource source)
         {
-            moveSpeed = Main.rand.Next(10, 14) * 2;
-            turnResistance = Main.rand.Next(12, 18) * 5;
+            moveSpeed = Main.rand.Next(12, 15) * 2;
+            turnResistance = Main.rand.Next(16, 19) * 5;
             positionOffset = new Vector2(Main.rand.Next(-3, 3), Main.rand.Next(-3, 3)) * 10;
             rotateDirection = Main.rand.NextBool() ? 1 : -1;
+            randomAmplitude = Main.rand.NextFloat(0.1f, 0.25f);
         }
 
         public override void AI()
@@ -55,15 +57,26 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
             NPC.TargetClosest();
             Player player = Main.player[NPC.target];
 
-            if (++AICounter % 150 == 0)
+            NPC.rotation = NPC.velocity.ToRotation() - MathHelper.PiOver2;
+
+            if (++AICounter < 150)
+            {
+                NPC.Move(player.Center, moveSpeed, turnResistance);
+            }
+            else if (AICounter == 150)
             {
                 rotateDirection = Main.rand.NextBool() ? 1 : -1;
-                positionOffset = new Vector2(Main.rand.Next(1, 4), Main.rand.Next(-3, 3)) * 15;
+                NPC.velocity = NPC.velocity.RotatedByRandom(MathHelper.PiOver4) * 2;
             }
 
-            NPC.rotation = NPC.velocity.ToRotation() - MathHelper.PiOver2;
-            NPC.Move(player.Center, moveSpeed, turnResistance);
-            NPC.velocity = NPC.velocity.RotatedBy(Math.Sin(AICounter * 5) * rotateDirection);
+            NPC.velocity = NPC.velocity.RotatedBy(Math.Sin(AICounter * randomAmplitude) * randomAmplitude * rotateDirection);
+
+            if (AICounter == 210) AICounter = 0;
+        }
+
+        public Vector2 PolarVector(float radius, float theta)
+        {
+            return new Vector2((float)Math.Cos(theta), (float)Math.Sin(theta)) * radius;
         }
 
         public override void FindFrame(int frameHeight)
@@ -71,6 +84,14 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
             if (++NPC.frameCounter % 5 == 0) NPC.frame.Y += frameHeight;
 
             if (NPC.frame.Y >= frameHeight * 2) NPC.frame.Y = 0;
+        }
+
+        public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Texture2D glow = ModContent.Request<Texture2D>(AssetDirectory.Boss + "Eye/MiniServant_Glow").Value;
+            Color color = Color.Lerp(Color.White, Color.Transparent, NPC.alpha / 255f);
+
+            spriteBatch.Draw(glow, NPC.Center - screenPos, null, color, NPC.rotation + MathHelper.PiOver2, glow.Size() / 2, NPC.scale, SpriteEffects.None, 0);
         }
     }
 }
