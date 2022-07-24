45ff44e1030b29891fabf07d8838d97e1685b13c
kjopnfer <gaypal0805@gmail.com>
2022-07-24 15:50:44 -0400

    Environment based shakeoff

--- Content/NPCs/Bosses/Eye/MiniServant.cs
+++ Content/NPCs/Bosses/Eye/MiniServant.cs
@@ -20,12 +20,10 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
         private float rotateDirection = 1;
         private float moveSpeed;
         private float turnResistance;
-        private Vector2 positionOffset;
 
         public Player latchPlayer;
-        private bool hasLatched = false;
         private Vector2 latchPoint;
-        private int latchCounter;
+        private int latchCounter = 0;
 
         private bool isLeft = false;
         private bool isRight = false;
@@ -63,7 +61,6 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
         {
             moveSpeed = Main.rand.Next(12, 15) * 2;
             turnResistance = Main.rand.Next(16, 19) * 5;
-            positionOffset = new Vector2(Main.rand.Next(-3, 3), Main.rand.Next(-3, 3)) * 10;
             rotateDirection = Main.rand.NextBool() ? 1 : -1;
             randomAmplitude = Main.rand.NextFloat(0.1f, 0.25f);
         }
@@ -72,6 +69,8 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
         {
             NPC.TargetClosest();
             Player player = Main.player[NPC.target];
+
+            NPC.dontTakeDamage = AICase == (int)AIStates.Latch;
 
             switch (AICase)
             {
@@ -98,7 +97,6 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
 
                         if (NPC.Hitbox.Intersects(latchTarget.getRect()))
                         {
-                            //hasLatched = true;
                             latchPoint = NPC.Center - player.Center;
                             latchPlayer = player;
 
@@ -112,40 +110,32 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
 
                     #region Shake Off Detection
                     // The reason that we don't use the control keys for left/right is to account for bottle-type accessories
-                    if (player.direction == 1)
+                    if (player.direction == 1 && !isRight)
                     {
-                        if (!isRight)
-                        {
-                            //Main.NewText("facing right");
-                            isRight = true;
-                            isLeft = false;
+                        isRight = true;
+                        isLeft = false;
 
-                            latchCounter += Main.rand.Next(3, 6);
-                        }
+                        latchCounter += Main.rand.Next(3, 6);
                     }
 
-                    if (player.direction == -1)
+                    if (player.direction == -1 && !isLeft)
                     {
-                        if (!isLeft)
-                        {
-                            //Main.NewText("facing left");
-                            isLeft = true;
-                            isRight = false;
+                        isLeft = true;
+                        isRight = false;
 
-                            latchCounter += Main.rand.Next(3, 6);
-                        }
+                        latchCounter += Main.rand.Next(3, 6);
                     }
 
                     if (latchPlayer.justJumped)
                     {
-                        //Main.NewText("jump");
-                        latchCounter += Main.rand.Next(3, 6);
+                        latchCounter += Main.rand.Next(4, 7);
                     }
 
-                    latchCounter -= Main.rand.Next(1, 3);
+                    if (latchCounter > 0 && AICounter++ % 3 == 0) latchCounter -= Main.rand.Next(1, 3);
                     if (latchCounter < 0) latchCounter = 0;
 
-                    if (latchCounter >= 60)
+                    Main.NewText(latchCounter);
+                    if (latchCounter >= 25 || player.wet || player.HasBuff(BuffID.OnFire))
                     {
                         NPC.velocity = Vector2.UnitX.RotatedByRandom(MathHelper.TwoPi) * 8;
                         latchCounter = 0;
@@ -165,92 +155,6 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Eye
                     }
                     break;
             }
-
-            /*if (!hasLatched)
-            {
-                NPC.rotation = NPC.velocity.ToRotation() - MathHelper.PiOver2;
-
-                if (++AICounter < 150)
-                {
-                    NPC.Move(player.Center, moveSpeed, turnResistance);
-                }
-                else if (AICounter == 150)
-                {
-                    rotateDirection = Main.rand.NextBool() ? 1 : -1;
-                    NPC.velocity = NPC.DirectionTo(player.Center).RotatedByRandom(MathHelper.PiOver4) * (moveSpeed / 2);
-                }
-
-                NPC.velocity = NPC.velocity.RotatedBy(Math.Sin(AICounter * randomAmplitude) * randomAmplitude * rotateDirection);
-
-                if (AICounter == 210) AICounter = 0;
-
-                foreach (Player latchTarget in Main.player)
-                {
-                    if (!latchTarget.active) continue;
-
-                    if (NPC.Hitbox.Intersects(latchTarget.getRect()))
-                    {
-                        hasLatched = true;
-                        latchPoint = NPC.Center - player.Center;
-                        latchPlayer = player;
-                    }
-                }
-            }
-            else
-            {
-                NPC.Center = latchPlayer.Center + latchPoint;
-
-                #region Shake Off Detection
-
-                //if(latchPlayer.releaseLeft && latchPlayer.oldDirection == 1) Main.NewText("left");
-                //if (latchPlayer.releaseRight && latchPlayer.oldDirection == -1) Main.NewText("right");
-
-                //if (latchPlayer.oldDirection == 1 && latchPlayer.direction == -1) Main.NewText("right");
-
-                // The reason that we don't use the control keys for left/right is to account for bottle-type accessories
-                if (player.direction == 1)
-                {
-                    if (!isRight)
-                    {
-                        Main.NewText("facing right");
-                        isRight = true;
-                        isLeft = false;
-
-                        latchCounter += Main.rand.Next(3, 6);
-                    }
-                }
-
-                if (player.direction == -1)
-                {
-                    if (!isLeft)
-                    {
-                        Main.NewText("facing left");
-
-                        isLeft = true;
-                        isRight = false;
-
-                        latchCounter += Main.rand.Next(3, 6);
-                    }
-                }
-
-                if (latchPlayer.justJumped)
-                {
-                    Main.NewText("jump");
-                    latchCounter += Main.rand.Next(3, 6);
-                }
-
-                if (latchCounter >= 40)
-                {
-                    latchCounter = 0;
-                    hasLatched = false;
-                }
-                #endregion
-            }*/
-        }
-
-        public Vector2 PolarVector(float radius, float theta)
-        {
-            return new Vector2((float)Math.Cos(theta), (float)Math.Sin(theta)) * radius;
         }
 
         public override void FindFrame(int frameHeight)
