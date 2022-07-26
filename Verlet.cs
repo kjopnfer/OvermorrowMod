85c2ecb0170ee5276e9480c398ea2a5e44720110
PeppaFan666 <treebarker2007@gmail.com>
2022-07-26 01:14:04 -0600

    no errors but points are in very odd positions, 20000 or so coords from each other

---
+++ Verlet.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using OvermorrowMod.Core;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using OvermorrowMod.Common.Primitives;
+using OvermorrowMod.Common.Primitives.Trails;
+using System;
+using System.Collections.Generic;
+
+namespace OvermorrowMod
+{
+	public class VerletPoint
+	{
+		public Vector2 prevPosition;
+		public Vector2 position;
+		public VerletPoint[] connections;
+		public bool locked;
+
+		public VerletPoint(Vector2 _prevPosition, Vector2 _position, VerletPoint[] _connections,bool _locked)
+        {
+			prevPosition = _prevPosition;
+			position = _position;
+			connections = _connections;
+			locked = _locked;
+        }
+	}
+
+
+
+	public class VerletStick
+	{
+		public VerletPoint point1;
+		public VerletPoint point2;
+		public float length;
+
+		public VerletStick(VerletPoint _point1, VerletPoint _point2)
+		{
+			point1 = _point1;
+			point2 = _point2;
+			length = Vector2.Distance(point1.position, point2.position);
+		}
+	}
+
+	static class Verlet
+	{
+		public static VerletPoint[] CalculateVerlet(VerletPoint[] points, Vector2 down, float delta, int depth = 10,float gravity = 100f)
+		{
+			int stickCount = 0;
+			VerletStick[] sticks = new VerletStick[2];
+			foreach (VerletPoint p in points)
+			{
+				if (!p.locked)
+				{
+					Vector2 positionBeforeUpdate = p.position;
+					p.position += p.position - p.prevPosition;
+					p.position += down * gravity * delta * delta;
+					p.prevPosition = positionBeforeUpdate;
+				}
+				
+				if (p.connections == null)
+                {
+					continue;
+                }
+				Main.NewText(p.connections[0]);
+				foreach (VerletPoint p2 in p.connections)
+				{
+					sticks[stickCount] = new VerletStick(p, p2);
+					stickCount++;
+				}
+
+			}
+			/*	var size = sticks.Length;
+				for (int i = 0; i < size; i++)
+				{
+					for (int j = i + 1; j < size; j++)
+					{
+						if (sticks[i] == sticks[j])
+						{
+							for (int k = j; k < size - 1; k++)
+							{
+								sticks[k] = sticks[k + 1];
+							}
+							j--;
+							size--;
+						}
+					}
+				}
+			*/
+			//var distinctArray = sticks[0..size];
+			//sticks = distinctArray;
+			
+			for (int i = 0; i < depth; i++)
+			{
+				foreach (VerletStick stick in sticks)
+				{
+					Main.NewText(stick.point1.position);
+					Vector2 stickCentre = (stick.point1.position + stick.point2.position) / 2;
+					Vector2 stickDir = Vector2.Normalize(stick.point1.position - stick.point2.position);
+					if (!stick.point1.locked)
+					{
+						stick.point1.position = stickCentre + stickDir * stick.length / 2;
+					}
+					if (!stick.point2.locked)
+					{
+						stick.point2.position = stickCentre - stickDir * stick.length / 2;
+					}
+
+				}
+			}
+			return points;
+
+		}
+
+	}
+}
---
+++ VerletTest.cs
@@ -0,0 +1,63 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static OvermorrowMod.Verlet;
+using Microsoft.Xna.Framework;
+
+namespace OvermorrowMod
+{
+    // Party Zombie is a pretty basic clone of a vanilla NPC. To learn how to further adapt vanilla NPC behaviors, see https://github.com/tModLoader/tModLoader/wiki/Advanced-Vanilla-Code-Adaption#example-npc-npc-clone-with-modified-projectile-hoplite
+    public class VerletTest : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("VerletTest");
+            // Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Zombie];
+            //Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Zombie];
+
+        }
+        public override string Texture => "OvermorrowMod/Assets/Textures/Perlin";
+        public override void SetDefaults()
+        {
+            NPC.width = 18;
+            NPC.height = 40;
+            NPC.damage = 14;
+            NPC.defense = 6;
+            NPC.lifeMax = 200;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath2;
+            NPC.value = 60f;
+            NPC.knockBackResist = 0.5f;
+            NPC.aiStyle = -1;
+            //aiType = NPCID.Zombie;
+           // animationType = NPCID.Zombie;
+           // banner = Item.NPCtoBanner(NPCID.Zombie);
+           // bannerItem = Item.BannerToItem(banner);
+        }
+        public VerletPoint[] points;
+        public override void AI()
+        {
+            if (NPC.ai[0] == 0)
+            {
+                points = new VerletPoint[3];
+                points[0] = new VerletPoint(new Vector2(0, 0), new Vector2(NPC.Center.X + 100, NPC.Center.Y + 25), null, true);
+                points[1] = new VerletPoint(new Vector2(0, 0), new Vector2(NPC.Center.X + 0, NPC.Center.Y + 25), null, false);
+                points[2] = new VerletPoint(new Vector2(0, 0), new Vector2(NPC.Center.X - 100, NPC.Center.Y + 25),null, false);
+                points[0].connections = new VerletPoint[1];
+                points[0].connections[0] = points[1];                
+                points[1].connections = new VerletPoint[1];
+                points[1].connections[0] = points[2];
+            }
+            points = CalculateVerlet(points,new Vector2(0,1),10);
+            NPC.ai[0]++;
+            foreach(VerletPoint point in points)
+            {
+                Dust.NewDust(point.position, 0, 0, DustID.GemEmerald, 0f, 0f, 100, default(Color), 0.25f);
+            }
+        }
+
+
+
+     
+    }
+}
