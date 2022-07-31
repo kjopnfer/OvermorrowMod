89bf6bebba867ff05a82e85059c87376c1510070
kjopnfer <gaypal0805@gmail.com>
2022-07-31 18:33:52 -0400

    Cart generation zone placeables spawn

---
+++ Content/Tiles/Carts/CartLamp.cs
@@ -0,0 +1,34 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace OvermorrowMod.Content.Tiles.Carts
+{
+    public class CartLamp : ModTile
+    {
+        public override bool CanExplode(int i, int j) => false;
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLighted[Type] = true;
+
+            TileObjectData.newTile.Width = 4;
+            TileObjectData.newTile.Height = 5;
+            TileObjectData.newTile.CoordinateHeights = new int[5] { 16, 16, 16, 16, 16 };
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.addTile(Type);
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Reaper Buff Station Thing");
+            AddMapEntry(Color.Red, name);
+        }
+    }
+}
---
+++ Content/WorldGeneration/CartGeneration.cs
@@ -0,0 +1,105 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using OvermorrowMod.Common.Base;
+using OvermorrowMod.Content.Tiles;
+using OvermorrowMod.Content.Tiles.Carts;
+using OvermorrowMod.Content.Tiles.DesertTemple;
+using OvermorrowMod.Core;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.WorldBuilding;
+
+namespace OvermorrowMod.Content.WorldGeneration
+{
+    public class CartGeneration : ModSystem
+    {
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            int DesertIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Micro Biomes"));
+            if (DesertIndex != -1)
+            {
+                tasks.Insert(DesertIndex + 1, new PassLegacy("Cart Zones", GenerateCarts));
+            }
+
+            base.ModifyWorldGenTasks(tasks, ref totalWeight);
+        }
+
+        private void GenerateCarts(GenerationProgress progress, GameConfiguration config)
+        {
+            progress.Message = "Setting up Merchant Zones";
+
+            // Places cart zones based on the size of the world
+            for (int i = 0; i < 3; i++)
+            {
+                bool validArea = false;
+
+                // Pick a random spot in the world that is in midair
+                int x = WorldGen.genRand.Next(0, Main.maxTilesX);
+                int y = WorldGen.genRand.Next((int)(Main.worldSurface * 0.35f), (int)(Main.worldSurface * 0.4f));
+
+                while (!validArea)
+                {
+                    // Loop downwards until we reach a solid tile
+                    Tile tile = Framing.GetTileSafely(x, y);
+                    while (!tile.HasTile)
+                    {
+                        y++;
+                        tile = Framing.GetTileSafely(x, y);
+                    }
+
+                    // We have the tile but we want to check if its a grass block, if it isn't restart the process
+                    if (tile.TileType == TileID.Grass && tile.WallType == WallID.None && tile.LiquidAmount == 0)
+                    {
+                        validArea = true;
+                    }
+                    else
+                    {
+                        // Pick a random spot in the world that is in midair
+                        x = WorldGen.genRand.Next(0, Main.maxTilesX);
+                        y = WorldGen.genRand.Next((int)(Main.worldSurface * 0.35f), (int)(Main.worldSurface * 0.5f));
+                    }
+                }
+
+                PlaceCart(x, y - 3);
+            }
+        }
+
+        public static void PlaceCart(int x, int y)
+        {
+            Dictionary<Color, int> TileMapping = new Dictionary<Color, int>
+            {
+                [new Color(102, 57, 49)] = ModContent.TileType<AstroGray>(),
+            };
+
+            Dictionary<Color, int> TileRemoval = new Dictionary<Color, int>
+            {
+                [new Color(0, 0, 0)] = -2
+            };
+
+            Texture2D ClearMap = ModContent.Request<Texture2D>(AssetDirectory.WorldGen + "Textures/CartZone").Value;
+            TexGen TileClear = BaseWorldGenTex.GetTexGenerator(ClearMap, TileRemoval, ClearMap, TileRemoval);
+            TileClear.Generate(x - (TileClear.width / 2), y - (TileClear.height), true, true);
+
+            Texture2D TileMap = ModContent.Request<Texture2D>(AssetDirectory.WorldGen + "Textures/CartZone").Value;
+            TexGen TileGen = BaseWorldGenTex.GetTexGenerator(TileMap, TileMapping);
+            TileGen.Generate(x - (TileClear.width / 2), y - (TileClear.height), true, true);
+
+
+            //ModUtils.PlaceObject(x + 1, y + 23, ModContent.TileType<CartSign>());
+            //ModUtils.PlaceObject(x + 1, y + 24, ModContent.TileType<CartSign>());
+            //ModUtils.PlaceObject(x + 1, y + 25, ModContent.TileType<CartSign>());
+            WorldGen.PlaceTile(x - (TileClear.width / 2), y - (TileClear.height) + 23, ModContent.TileType<CartSign>());
+
+            //WorldGen.PlaceTile(x - (TileClear.width / 2) + 11, y - (TileClear.height) + 17, TileID.Adamantite);
+            WorldGen.PlaceTile(x - (TileClear.width / 2) + 12, y - (TileClear.height) + 19, ModContent.TileType<CartLamp>());
+          
+            //WorldGen.PlaceTile(x - (TileClear.width / 2) + 1, y - (TileClear.height) + 24, TileID.Adamantite);
+            //WorldGen.PlaceTile(x - (TileClear.width / 2) + 1, y - (TileClear.height) + 25, TileID.Titanium);
+        }
+    }
+}
