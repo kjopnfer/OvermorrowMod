44b79a89f9123904d2600cb635087190213f417d
kjopnfer <gaypal0805@gmail.com>
2022-08-07 13:01:27 -0400

    Made a bunch of files and didn't do anything

---
+++ Common/Cutscenes/DialoguePlayer.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System.Collections.Generic;
+using Terraria.UI;
+using Terraria.GameInput;
+using Terraria.GameContent.UI.Elements;
+using ReLogic.Content;
+
+namespace OvermorrowMod.Common.Cutscenes
+{
+    public class DialoguePlayer : ModPlayer
+    {
+
+    }
+}
---
+++ Common/Cutscenes/DialogueSystem.cs
@@ -0,0 +1,36 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System.Collections.Generic;
+using Terraria.UI;
+using Terraria.GameInput;
+using Terraria.GameContent.UI.Elements;
+using ReLogic.Content;
+
+namespace OvermorrowMod.Common.Cutscenes
+{
+    public class DialogueSystem : ModSystem
+    {
+        public override void Load()
+        {
+            base.Load();
+        }
+
+        public override void Unload()
+        {
+            base.Unload();
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            base.ModifyInterfaceLayers(layers);
+        }
+
+        public override void UpdateUI(GameTime gameTime)
+        {
+            base.UpdateUI(gameTime);
+        }
+    }
+}
