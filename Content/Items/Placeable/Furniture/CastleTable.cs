0d48323f6a8ba3a274e598613b14f75fde577947
kjopnfer <gaypal0805@gmail.com>
2022-08-17 22:26:38 -0400

    Added the town tables

---
+++ Content/Items/Placeable/Furniture/CastleTable.cs
@@ -0,0 +1,52 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Block = OvermorrowMod.Content.Tiles.Carts.CastleTable;
+
+namespace OvermorrowMod.Content.Items.Placeable.Furniture
+{
+    public class CastleTable : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Castle Table");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 20;
+            Item.maxStack = 99;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = 2000;
+            Item.createTile = ModContent.TileType<Block>();
+        }
+    }
+
+    public class CastleTable_Alt : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Castle Table");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 20;
+            Item.maxStack = 99;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = 2000;
+            Item.createTile = ModContent.TileType<Content.Tiles.Carts.CastleTable_Alt>();
+        }
+    }
+}
---
+++ Content/Tiles/Carts/CastleTable.cs
@@ -0,0 +1,70 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using OvermorrowMod.Content.NPCs.Carts;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace OvermorrowMod.Content.Tiles.Carts
+{
+    public class CastleTable : ModTile
+    {
+        public override bool CanExplode(int i, int j) => false;
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileLighted[Type] = true;
+
+            TileObjectData.newTile.Width = 3;
+            TileObjectData.newTile.Height = 2;
+            TileObjectData.newTile.CoordinateHeights = new int[2] { 16, 16 };
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(78, 67, 62));
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 144, 80, ModContent.ItemType<Items.Placeable.Furniture.CastleTable>());
+        }
+    }
+
+    public class CastleTable_Alt : ModTile
+    {
+        public override bool CanExplode(int i, int j) => false;
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileLighted[Type] = true;
+
+            TileObjectData.newTile.Width = 3;
+            TileObjectData.newTile.Height = 2;
+            TileObjectData.newTile.CoordinateHeights = new int[2] { 16, 16 };
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(78, 67, 62));
+        }
+
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 144, 80, ModContent.ItemType<Items.Placeable.Furniture.CastleTable>());
+        }
+    }
+}
