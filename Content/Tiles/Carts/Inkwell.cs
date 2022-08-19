54cf72c6441f64664f3dbaad31099bb9ddd85963
kjopnfer <gaypal0805@gmail.com>
2022-08-18 20:34:27 -0400

    Added the inkwell tile

---
+++ Content/Tiles/Carts/Inkwell.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace OvermorrowMod.Content.Tiles.Carts
+{
+    public class Inkwell : ModTile
+    {
+        public override bool CanExplode(int i, int j) => false;
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLighted[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.StyleOnTable1x1);
+            TileObjectData.newTile.Width = 1;
+            TileObjectData.newTile.Height = 2;
+            TileObjectData.newTile.CoordinateHeights = new int[2] { 16, 16 };
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(78, 67, 62));
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 14, 34, ModContent.ItemType<Content.Items.Placeable.Furniture.Inkwell>());
+        }
+    }
+}
