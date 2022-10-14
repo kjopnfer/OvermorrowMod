ca73f8467e935c169d8cb8b13cc837bd981b56ed
kjopnfer <gaypal0805@gmail.com>
2022-10-13 22:44:33 -0400

    Start of custom fighter AI and bow firing frames

--- Content/NPCs/Bosses/Bandits/ArcherBandit.cs
+++ Content/NPCs/Bosses/Bandits/ArcherBandit.cs
@@ -25,12 +25,9 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Bandits
 
         public override void SetDefaults()
         {
-            NPC.aiStyle = 3;
-            AIType = NPCID.GoblinScout;
-
             NPC.width = 22;
             NPC.height = 44;
-
+            NPC.aiStyle = -1;
             NPC.damage = 21;
             NPC.defense = 12;
             NPC.lifeMax = 340;
@@ -41,13 +38,93 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Bandits
             NPC.npcSlots = 10f;
         }
 
+        private ref float AIState => ref NPC.ai[0];
+        private ref float AICounter => ref NPC.ai[1];
+
+        private enum AIStates
+        {
+            Walk = 0,
+            DrawBow = 1
+        }
+
+        public void Move(Vector2 targetPosition, float moveSpeed, float maxSpeed, float jumpSpeed)
+        {
+            if (NPC.Center.X < targetPosition.X)
+            {
+                NPC.velocity.X += moveSpeed;
+
+                if (NPC.velocity.X > maxSpeed) NPC.velocity.X = maxSpeed;
+            }
+            else if (NPC.Center.X > targetPosition.X)
+            {
+                NPC.velocity.X -= moveSpeed;
+
+                if (NPC.velocity.X < -maxSpeed) NPC.velocity.X = -maxSpeed;
+            }
+
+            if (NPC.collideY && NPC.velocity.Y == 0)
+            {
+                Collision.StepUp(ref NPC.position, ref NPC.velocity, NPC.width, NPC.height, ref NPC.stepSpeed, ref NPC.gfxOffY, 1, false, 0);
+
+                #region Jump Handling
+                if (NPC.collideX || CheckGap())
+                {
+                    NPC.velocity.Y += jumpSpeed;
+                }
+
+                #endregion
+            }
+        }
+
+        private bool CheckGap()
+        {
+            Rectangle npcHitbox = NPC.getRect();
+
+            Vector2 checkLeft = new Vector2(npcHitbox.BottomLeft().X, npcHitbox.BottomLeft().Y);
+            Vector2 checkRight = new Vector2(npcHitbox.BottomRight().X, npcHitbox.BottomRight().Y);
+            Vector2 hitboxDetection = (NPC.velocity.X < 0 ? checkLeft : checkRight) / 16;
+
+            int directionOffset = NPC.direction;
+
+            Tile tile = Framing.GetTileSafely((int)hitboxDetection.X + directionOffset, (int)hitboxDetection.Y);
+
+            return !tile.HasTile;
+        }
+
         public override void AI()
         {
             NPC.TargetClosest();
             Player target = Main.player[NPC.target];
 
-            FrameUpdate(FrameType.Walk);
-            Collision.StepUp(ref NPC.position, ref NPC.velocity, NPC.width, NPC.height, ref NPC.stepSpeed, ref NPC.gfxOffY, 1, false, 0);
+            switch (AIState)
+            {
+                case (int)AIStates.Walk:
+                    Move(target.Center, 0.2f, 1f, 2f);
+                    FrameUpdate(FrameType.Walk);
+
+                    if (AICounter++ == 240)
+                    {
+                        AIState = (int)AIStates.DrawBow;
+                        AICounter = 0;
+                    }
+
+                    break;
+                case (int)AIStates.DrawBow:
+                    NPC.velocity = Vector2.Zero;
+                    NPC.aiStyle = -1;
+
+                    if (FrameUpdate(FrameType.DrawBow))
+                    {
+                        // Handle shooting here
+                    }
+                    else
+                    {
+                        AIState = (int)AIStates.Walk;
+                        AICounter = 0;
+                    }
+
+                    break;
+            }
         }
 
         int xFrame = 1;
@@ -66,12 +143,12 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Bandits
         private enum FrameType
         {
             Walk,
-            Jump, 
+            Jump,
             DrawBow
         }
 
         float tempCounter = 0;
-        private void FrameUpdate(FrameType type)
+        private bool FrameUpdate(FrameType type)
         {
             switch (type)
             {
@@ -95,14 +172,41 @@ namespace OvermorrowMod.Content.NPCs.Bosses.Bandits
                         // Change the walking frame at a speed depending on the velocity
                         int walkRate = (int)Math.Round(Math.Abs(NPC.velocity.X));
                         tempCounter += walkRate;
-                        if (tempCounter > 8)
+                        if (tempCounter > 5)
                         {
                             yFrame++;
                             tempCounter = 0;
                         }
                     }
                     break;
+                case FrameType.DrawBow:
+                    xFrame = 1;
+
+                    if (tempCounter++ < 60) // NPC stands in the ready position prior to drawing back the bow
+                    {
+                        if (yFrame > 0) yFrame = 0;
+                    }
+                    else
+                    {
+                        if (tempCounter > 68)
+                        {
+                            if (yFrame == 10)
+                            {
+                                yFrame = 0;
+                                tempCounter = 0;
+
+                                return false;
+                            }
+
+                            yFrame++;
+                            tempCounter = 60;
+                        }
+                    }
+
+                    break;
             }
+
+            return true;
         }
 
         public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
