fcab60538038d2f7641a9af02f47e1f1ce1f68a5
kjopnfer <gaypal0805@gmail.com>
2023-01-08 00:34:35 -0500

    Added BowStats class

--- Common/VanillaOverrides/Bow/GlobalBow.cs
+++ Common/VanillaOverrides/Bow/GlobalBow.cs
@@ -7,19 +7,49 @@ using System.Linq;
 
 namespace OvermorrowMod.Common.VanillaOverrides.Bow
 {
+    public class BowStats
+    {
+        public int BowType;
+        public int BowDamage;
+
+        public BowStats(int BowType, int BowDamage)
+        {
+            this.BowType = BowType;
+            this.BowDamage = BowDamage;
+        }
+    }
+
     public class GlobalBow : GlobalItem
     {
         public override bool InstancePerEntity => true;
-        public Dictionary<int, int> OverridedBows = new Dictionary<int, int>()
+        public Dictionary<int, BowStats> OverridedBows = new Dictionary<int, BowStats>()
         {
-            { ItemID.DemonBow, ModContent.ProjectileType<TestBow_Held>() }
+            { ItemID.WoodenBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 4) },
+            { ItemID.BorealWoodBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 6) },
+            { ItemID.RichMahoganyBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 6) },
+            { ItemID.PalmWoodBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 6) },
+            { ItemID.EbonwoodBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 8) },
+            { ItemID.ShadewoodBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 8) },
+            { ItemID.CopperBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 6) },
+            { ItemID.TinBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 7) },
+            { ItemID.IronBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 8) },
+            { ItemID.LeadBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 9) },
+            { ItemID.SilverBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 9) },
+            { ItemID.TungstenBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 10) },
+            { ItemID.GoldBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 11) },
+            { ItemID.PlatinumBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 13) },
+            { ItemID.DemonBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 14) },
+            { ItemID.TendonBow, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 19) },
+            { ItemID.MoltenFury, new BowStats(ModContent.ProjectileType<TestBow_Held>(), 31) },
         };
 
         public override void SetDefaults(Item item)
         {
             if (OverridedBows.ContainsKey(item.type))
             {
-                item.shoot = OverridedBows[item.type];
+                BowStats bow = OverridedBows[item.type];
+                item.shoot = bow.BowType;
+                item.damage = bow.BowDamage;
                 item.noUseGraphic = true;
             }
         }
