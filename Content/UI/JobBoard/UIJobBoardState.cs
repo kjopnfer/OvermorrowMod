899fcb845604794786ca7ef32ece9905ca4dc124
kjopnfer <gaypal0805@gmail.com>
2023-06-17 22:51:05 -0400

    Button and Back Panel classes

--- Content/UI/JobBoard/UIJobBoardState.cs
+++ Content/UI/JobBoard/UIJobBoardState.cs
@@ -30,11 +30,22 @@ namespace OvermorrowMod.Content.UI.JobBoard
 
         // there will be unique job boards tied to various towns
         // therefore should load and save jobs tied to the job board
+        private UIJobBoardPanel drawSpace = new UIJobBoardPanel();
+        internal UIJobBoardCloseButton closeButton = new UIJobBoardCloseButton();
 
-        private bool showBoard = false;
+        public bool showBoard = false;
         public void OpenJobBoard()
         {
             showBoard = true;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (!showBoard) return;
+
+            this.RemoveAllChildren();
+            ModUtils.AddElement(drawSpace, Main.screenWidth / 2 - 375, Main.screenHeight / 2 - 250, 750, 500, this);
+            ModUtils.AddElement(closeButton, 700, 0, 22, 22, drawSpace);
         }
 
         /// <summary>
@@ -52,4 +63,46 @@ namespace OvermorrowMod.Content.UI.JobBoard
         /// </summary>
         private void OnBoardClose() { }
     }
+
+    public class UIJobBoardPanel : UIPanel
+    {
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>(AssetDirectory.UI + "BookBack").Value;
+            spriteBatch.Draw(texture, GetDimensions().Center(), null, Color.White, 0, texture.Size() / 2f, 1f, 0, 0);
+        }
+    }
+
+    public class UIJobBoardCloseButton : UIElement
+    {
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 pos = GetDimensions().ToRectangle().TopLeft();
+            bool isHovering = ContainsPoint(Main.MouseScreen);
+
+            if (Parent.Parent is UIJobBoardState parent)
+            {
+                Texture2D texture = ModContent.Request<Texture2D>(AssetDirectory.UI + "BookClose").Value;
+                Color color = isHovering ? Color.White * 0.5f : Color.White;
+                spriteBatch.Draw(texture, pos + new Vector2(texture.Width / 2f, texture.Height / 2f), null, color, 0f, texture.Size() / 2f, 1f, 0, 0);
+            }
+        }
+
+        public override void MouseDown(UIMouseEvent evt)
+        {
+            SoundEngine.PlaySound(SoundID.MenuTick);
+
+            if (Parent.Parent is UIJobBoardState parent)
+            {
+                parent.RemoveAllChildren();
+                parent.showBoard = false;
+            }
+        }
+    }
+
 }
