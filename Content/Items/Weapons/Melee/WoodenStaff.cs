9ea032eec146b9826b3f517add2c86312879c06f
kjopnfer <gaypal0805@gmail.com>
2023-07-22 15:47:57 -0400

    Swing test

--- Content/Items/Weapons/Melee/WoodenStaff.cs
+++ Content/Items/Weapons/Melee/WoodenStaff.cs
@@ -67,7 +67,7 @@ namespace OvermorrowMod.Content.Items.Weapons.Melee
         }
     }
 
-    /*public class WoodenStaff_Held : ModProjectile
+    public class WoodenStaff_Held : ModProjectile
     {
         public override void SetStaticDefaults()
         {
@@ -87,27 +87,110 @@ namespace OvermorrowMod.Content.Items.Weapons.Melee
         }
 
         Player player => Main.player[Projectile.owner];
+
+        public ref float AICounter => ref Projectile.ai[0];
+
+        /// <summary>
+        /// How long the player has held down the weapon, separate from the counter that handles actions
+        /// </summary>
+        public ref float HoldCounter => ref Projectile.ai[1];
+
         public override void AI()
         {
             if (player.active && player.HeldItem.type == ModContent.ItemType<WoodenStaff>()) Projectile.timeLeft = 10;
 
             Projectile.width = Projectile.height = 40;
-
             player.heldProj = Projectile.whoAmI;
 
-            float staffRotation = player.Center.DirectionTo(Main.MouseWorld).ToRotation();
+            HandleArmDrawing();
+            HandleWeaponUse();
+        }
+
+
+        private void HandleWeaponUse()
+        {
+            // The weapon will always default to the light attack animations
+            // Meaning clicking once will be enough for the light attack animations to go through their entire movement cycle
+            // However, holding down click will stop the movement cycle halfway through until the player releases
+
+            // Prevent weapon from being used again while in release state
+            if (player.controlUseItem && !justReleasedWeapon) HandleWeaponHold();
+
+            if (!player.controlUseItem && HoldCounter > 0) HandleWeaponRelease();
+        }
+
+        public int maxHoldCount = 60;
+        private void HandleWeaponHold()
+        {
+            if (HoldCounter < maxHoldCount) Main.NewText(HoldCounter++);
+
+        }
+
+        
+        private bool justReleasedWeapon = false;
+        private bool inReleaseState = false;
+        private void HandleWeaponRelease()
+        {
+
+            // On weapon release is when we execute the attack animation
+            if (!justReleasedWeapon)
+            {
+                justReleasedWeapon = true;
+
+                if (HoldCounter < 15) Main.NewText("light attack");
+                if (HoldCounter >= 15) Main.NewText("heavy attack");
+            }
+
+            float backTime = 20;
+            float forwardTime = 20;
+            float holdTime = 10;
+
+            AICounter++;
+            if (AICounter <= backTime) 
+                swingAngle = MathHelper.Lerp(0, 100, ModUtils.EaseOutQuint(Utils.Clamp(AICounter, 0, backTime) / backTime));
+
+            if (AICounter > backTime && AICounter <= backTime + forwardTime) 
+                swingAngle = MathHelper.Lerp(100, -75, ModUtils.EaseInCubic(Utils.Clamp(AICounter - backTime, 0, forwardTime) / forwardTime));
+
+            if (AICounter > backTime + forwardTime && AICounter <= backTime + forwardTime + holdTime)
+                swingAngle = MathHelper.Lerp(-75, 0, ModUtils.EaseInQuart(Utils.Clamp(AICounter - (backTime + forwardTime), 0, holdTime) / holdTime));
+
+            // HoldCounter gets reset to zero at the end of the attack animation
+            if (AICounter >= backTime + forwardTime + holdTime)
+            {
+                AICounter = 0;
+                HoldCounter = 0;
+                swingAngle = 0;
+
+                justReleasedWeapon = false;
+            }
+        }
+
+        private void HandleWeaponDrawing(Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+            var spriteEffects = player.direction == 1 ? SpriteEffects.FlipVertically : SpriteEffects.None;
+
+            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, lightColor, Projectile.rotation, texture.Size() / 2f, Projectile.scale, spriteEffects, 1);
+        }
+
+        public float swingAngle = 0;
+        private void HandleArmDrawing()
+        {
+            float staffRotation = player.Center.DirectionTo(Main.MouseWorld).ToRotation() + MathHelper.ToRadians(swingAngle) * -player.direction;
+            Main.NewText(staffRotation);
 
             Projectile.rotation = staffRotation;
-            Projectile.spriteDirection = staffRotation > MathHelper.PiOver2 || staffRotation < -MathHelper.PiOver2 ? -1 : 1;
+            Projectile.spriteDirection = Main.MouseWorld.X < player.Center.X ? -1 : 1;
             player.direction = Projectile.spriteDirection;
 
-            //Vector2 positionOffset = (player.direction == -1 ? PositionOffset.Item1 : PositionOffset.Item2).RotatedBy(gunRotation);
             Vector2 positionOffset = (player.direction == -1 ? new Vector2(14, -4) : new Vector2(10, 6)).RotatedBy(staffRotation);
             Projectile.Center = player.RotatedRelativePoint(player.MountedCenter + positionOffset);
 
             float backRotation = player.direction == -1 ? -150 : -30;
             player.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation + MathHelper.ToRadians(backRotation));
             player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation + MathHelper.ToRadians(-90));
+
         }
 
         public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
@@ -117,79 +200,14 @@ namespace OvermorrowMod.Content.Items.Weapons.Melee
 
         public override void Kill(int timeLeft)
         {
-            Main.NewText("dide");
             base.Kill(timeLeft);
         }
 
         public override bool PreDraw(ref Color lightColor)
         {
-            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-            var spriteEffects = player.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipVertically;
-
-            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, lightColor, Projectile.rotation, texture.Size() / 2f, Projectile.scale, spriteEffects, 1);
+            HandleWeaponDrawing(lightColor);
 
             return false;
-        }
-    }*/
-
-    public class WoodenStaff_Held : ModProjectile
-    {
-        // Define the range of the Spear Projectile. These are overrideable properties, in case you'll want to make a class inheriting from this one.
-        protected virtual float HoldoutRangeMin => 24f;
-        protected virtual float HoldoutRangeMax => 96f;
-
-        public override void SetDefaults()
-        {
-            Projectile.CloneDefaults(ProjectileID.Spear); // Clone the default values for a vanilla spear. Spear specific values set for width, height, aiStyle, friendly, penetrate, tileCollide, scale, hide, ownerHitCheck, and melee.
-            Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = -1;
-        }
-
-        public override bool PreAI()
-        {
-            Player player = Main.player[Projectile.owner]; // Since we access the owner player instance so much, it's useful to create a helper local variable for this
-            int duration = player.itemAnimationMax; // Define the duration the projectile will exist in frames
-
-            player.heldProj = Projectile.whoAmI; // Update the player's held projectile id
-
-            // Reset projectile time left if necessary
-            if (Projectile.timeLeft > duration)
-            {
-                Projectile.timeLeft = duration;
-            }
-
-            //Projectile.velocity = Vector2.Normalize(Projectile.velocity); // Velocity isn't used in this spear implementation, but we use the field to store the spear's attack direction.
-            Vector2 attackDirection = Vector2.Normalize(Projectile.velocity);
-
-            float halfDuration = duration * 0.5f;
-            float progress;
-
-            // Here 'progress' is set to a value that goes from 0.0 to 1.0 and back during the item use animation.
-            if (Projectile.timeLeft < halfDuration)
-            {
-                progress = Projectile.timeLeft / halfDuration;
-            }
-            else
-            {
-                progress = (duration - Projectile.timeLeft) / halfDuration;
-            }
-
-            // Move the projectile from the HoldoutRangeMin to the HoldoutRangeMax and back, using SmoothStep for easing the movement
-            Projectile.Center = player.MountedCenter + Vector2.SmoothStep(attackDirection * HoldoutRangeMin, attackDirection * HoldoutRangeMax, progress);
-
-            // Apply proper rotation to the sprite.
-            if (Projectile.spriteDirection == -1)
-            {
-                // If sprite is facing left, rotate 45 degrees
-                Projectile.rotation += MathHelper.ToRadians(45f);
-            }
-            else
-            {
-                // If sprite is facing right, rotate 135 degrees
-                Projectile.rotation += MathHelper.ToRadians(135f);
-            }
-
-            return false; // Don't execute vanilla AI.
         }
     }
 }
