683a1045e83cbf3a60767152a8506a6b77a32767
kjopnfer <gaypal0805@gmail.com>
2023-08-16 22:32:58 -0400

    Wrote TODOs and thats about it

--- Common/BackgroundObjects/BackgroundTree.cs
+++ Common/BackgroundObjects/BackgroundTree.cs
@@ -1,13 +1,43 @@
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.ModLoader.IO;
 
 namespace OvermorrowMod.Common.BackgroundObjects
 {
+    // TODO: On creation, assign a list of tree segment textures that are randomly generated and then saved
     public class BackgroundTree : BaseBackgroundObject
     {
+        /// <summary>
+        /// The tree's height in tiles
+        /// </summary>
+        public int TreeHeight = 10;
+
+        /// <summary>
+        /// The tree texture segments with the base of the tree as index 0
+        /// </summary>
+        public List<string> TreeSegments = new List<string>();
+
+        public override void LoadData(TagCompound tag)
+        {
+            // TODO: Save TreeHeight and TreeSegments
+            base.LoadData(tag);
+        }
+
+        public override void SaveData(TagCompound tag)
+        {
+            base.SaveData(tag);
+        }
+
+
         public override bool BackgroundObjectTileCheck(int x, int y)
         {
             Tile tile = Main.tile[x, y];
             return tile.HasTile;
+        }
+
+        public override void DrawObject(SpriteBatch spriteBatch)
+        {
+            base.DrawObject(spriteBatch);
         }
 
         public override (float, float) Size() => new(420, 420);
