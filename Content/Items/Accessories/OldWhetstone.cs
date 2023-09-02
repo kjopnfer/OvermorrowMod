a723b14bff0744317a0a1dce9e2a7b7d3a6c7a33
kjopnfer <gaypal0805@gmail.com>
2023-09-01 22:52:08 -0400

    Added Old Whetstone

--- Content/Items/Accessories/OldWhetstone.cs
+++ Content/Items/Accessories/OldWhetstone.cs
@@ -5,20 +5,20 @@ using Terraria.ModLoader;
 
 namespace OvermorrowMod.Content.Items.Accessories
 {
-    public class ImbuementPouch : ModItem
+    public class OldWhetstone : ModItem
     {
         public override void SetDefaults()
         {
             Item.width = 32;
             Item.height = 32;
             Item.value = Item.buyPrice(0, 1, 50, 0);
-            Item.rare = ItemRarityID.Orange;
+            Item.rare = ItemRarityID.Blue;
             Item.accessory = true;
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.GetModPlayer<OvermorrowModPlayer>().ImbuementPouch = true;
+            player.GetCritChance(DamageClass.Generic) += player.statLifeMax2 / 25f;
         }
     }
 }
