5ccc5684f8e1c7882d88f8c6b094d4c50cb548c4
kjopnfer <gaypal0805@gmail.com>
2024-01-03 23:33:55 -0500

    Initialize UI in ModSystem

---
+++ Content/UI/SpeechBubble/UISpeechBubbleSystem.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using Terraria.UI;
+
+namespace OvermorrowMod.Content.UI.SpeechBubble
+{
+    public class UISpeechBubbleSystem : ModSystem
+    {
+        UserInterface SpeechBubbleUI;
+        internal UISpeechBubbleState SpeechBubbleState;
+
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                SpeechBubbleState = new UISpeechBubbleState();
+                SpeechBubbleUI = new UserInterface();
+                SpeechBubbleUI.SetState(SpeechBubbleState);
+            }
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int ResourceBars = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Resource Bars"));
+            if (ResourceBars != -1)
+            {
+                layers.Insert(ResourceBars, new LegacyGameInterfaceLayer(
+                    "OvermorrowMod: SpeechBubble UI",
+                    delegate
+                    {
+                        SpeechBubbleUI.Draw(Main.spriteBatch, new GameTime());
+                        return true;
+                    },
+                    InterfaceScaleType.Game)
+                );
+            }
+        }
+
+        public override void UpdateUI(GameTime gameTime)
+        {
+            SpeechBubbleUI?.Update(gameTime);
+        }
+    }
+}
