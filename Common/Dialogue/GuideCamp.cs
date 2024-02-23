a1d91ea62f0ee6e7bbf22ee8d46e6f3b05129541
kjopnfer <gaypal0805@gmail.com>
2024-02-22 21:35:34 -0500

    Convert old guide XML dialogue to new system

--- Common/Dialogue/GuideCamp.cs
+++ Common/Dialogue/GuideCamp.cs
@@ -27,7 +27,7 @@ namespace OvermorrowMod.Common.Dialogue
                     },
                     new DialogueChoice[]
                     {
-                        new DialogueChoice("Fine", null, StartQuest)
+                        new DialogueChoice(ModContent.Request<Texture2D>(AssetDirectory.UI + "Dialogue_QuestIcon", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, "Fine", null, StartQuest)
                     }
                 ),
 
@@ -54,15 +54,81 @@ namespace OvermorrowMod.Common.Dialogue
                     },
                     new DialogueChoice[]
                     {
-                        new DialogueChoice("Take on a powerful monster", "quest_complete_monster"),
-                        new DialogueChoice("Discover an uncharted biome", "quest_complete_biome"),
-                        new DialogueChoice("Build a cozy place to live", "quest_complete_build"),
+                        new DialogueChoice(ModContent.Request<Texture2D>(AssetDirectory.UI + "Dialogue_ChestIcon", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, "Take on a powerful monster", "quest_complete_monster"),
+                        new DialogueChoice(ModContent.Request<Texture2D>(AssetDirectory.UI + "Dialogue_ChestIcon", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, "Discover an uncharted biome", "quest_complete_biome"),
+                        new DialogueChoice(ModContent.Request<Texture2D>(AssetDirectory.UI + "Dialogue_ChestIcon", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, "Build a cozy place to live", "quest_complete_build"),
                     }
                 ),
 
+                new DialogueNode(
+                    "quest_complete_monster",
+                    new Text[]
+                    {
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "Guide", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "A monster eh? I have heard of some {giant floating eye} terrorizing travelers at night...", 180),
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "Guide", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "Maybe look for a [caravan merchant] or someone who can confirm the rumors.", 60),
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "Guide", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "Regardless, you'll have to get some better gear before you try anything like that. I don't get out much these days so why don't you borrow these for now. The rest you'll have to find on your own.", 240)
+                    },
+                    new DialogueChoice[]
+                    {
+                        new DialogueChoice(ModContent.Request<Texture2D>(AssetDirectory.UI + "Dialogue_QuestIcon", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, "Turn In", null, QuestRewardMonster)
+                    }
+                ),
 
+                new DialogueNode(
+                    "quest_complete_biome",
+                    new Text[]
+                    {
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "Guide", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "Quite the explorer you are! Well if uncharted biomes are what you're looking for you couldn't have come to a better place.", 180),
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "Guide", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "There's a place they call the {Crimson} that no adventurer dares enter.", 90),
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "Guide", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "Regardless, you'll have to get some better gear before you try anything like that. I don't get out much these days so why don't you borrow these for now. The rest you'll have to find on your own.", 240)
+                    },
+                    new DialogueChoice[]
+                    {
+                        new DialogueChoice(ModContent.Request<Texture2D>(AssetDirectory.UI + "Dialogue_QuestIcon", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, "Turn In", null, QuestRewardBiome)
+                    }
+                ),
+
+                new DialogueNode(
+                    "quest_complete_build",
+                    new Text[]
+                    {
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "Guide", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "Ah, settling down are we? You think you could find a place for these in that fancy new home of ours? Things start to get heavy after so many years.", 180),
+                    },
+                    new DialogueChoice[]
+                    {
+                        new DialogueChoice("Don't get excited, it won't be anything too fancy", "quest_complete_build_end"),
+                        new DialogueChoice("I'll make them the best storage space you've ever seen", "quest_complete_build_end"),
+                    }
+                ),
+
+                new DialogueNode(
+                    "quest_complete_build_end",
+                    new Text[]
+                    {
+                        new Text(
+                            ModContent.Request<Texture2D>(AssetDirectory.Portrait + "GuideWave", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value,
+                            "I'm sure whatever you build it will be great. Stay safe out there!", 60),
+                    },
+                    new DialogueChoice[]
+                    {
+                        new DialogueChoice(ModContent.Request<Texture2D>(AssetDirectory.UI + "Dialogue_QuestIcon", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, "Turn In", null, QuestRewardBuild)
+                    }
+                ),
             };
-
         }
 
         private void StartQuest(Player player, NPC npc)
@@ -84,6 +150,30 @@ namespace OvermorrowMod.Common.Dialogue
 
             // Run the Quest Accepted UI
             Main.NewText("ACCEPTED QUEST: " + quest.QuestName, Microsoft.Xna.Framework.Color.Yellow);
+        }
+
+        private void QuestRewardBuild(Player player, NPC npc)
+        {
+            QuestPlayer questPlayer = player.GetModPlayer<QuestPlayer>();
+            var quest = npc.GetGlobalNPC<QuestNPC>().GetCurrentQuest(npc, out var isDoing);
+
+            questPlayer.CompleteQuest(quest.QuestID, "build_reward");
+        }
+
+        private void QuestRewardMonster(Player player, NPC npc)
+        {
+            QuestPlayer questPlayer = player.GetModPlayer<QuestPlayer>();
+            var quest = npc.GetGlobalNPC<QuestNPC>().GetCurrentQuest(npc, out var isDoing);
+
+            questPlayer.CompleteQuest(quest.QuestID, "monster_reward");
+        }
+
+        private void QuestRewardBiome(Player player, NPC npc)
+        {
+            QuestPlayer questPlayer = player.GetModPlayer<QuestPlayer>();
+            var quest = npc.GetGlobalNPC<QuestNPC>().GetCurrentQuest(npc, out var isDoing);
+
+            questPlayer.CompleteQuest(quest.QuestID, "biome_reward");
         }
     }
 }
