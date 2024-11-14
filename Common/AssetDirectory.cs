1ec0bdcd3c8c30884ee744aa519d083aa414543b
kjopnfer <gaypal0805@gmail.com>
2024-11-13 23:16:41 -0500

    update assetdirectory

--- Common/AssetDirectory.cs
+++ Common/AssetDirectory.cs
@@ -2,11 +2,17 @@ namespace OvermorrowMod.Common
 {
     public static class AssetDirectory
     {
+        #region Base
         public const string Assets = "OvermorrowMod/Assets/";
 
         public const string Items = Assets + "Sprites/Items/";
 
+        public const string Tiles = Assets + "Tiles/";
+
         // For TexGen mapping, not shader mapping
         public const string TextureMaps = Assets + "Maps/";
+        #endregion
+
+        public const string ArchiveTiles = Tiles + "Archives/";
     }
 }
