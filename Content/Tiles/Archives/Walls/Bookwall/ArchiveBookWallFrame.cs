7d237cf0a75d3f416071dd98cd70c31b4536779b
kjopnfer <gaypal0805@gmail.com>
2024-11-29 14:34:17 -0500

    update book wall texture

--- Content/Tiles/Archives/Walls/Bookwall/ArchiveBookWallFrame.cs
+++ Content/Tiles/Archives/Walls/Bookwall/ArchiveBookWallFrame.cs
@@ -24,11 +24,29 @@ namespace OvermorrowMod.Content.Tiles.Archives
             return false;
         }
 
+        void DrawVerticalFrameSegment(Texture2D texture, int i, int j)
+        {
+            Lighting.GetCornerColors(i, j, out VertexColors vertices);
+
+            Vector2 drawPosition = new Vector2(i * 16 - (int)Main.screenPosition.X + Main.offScreenRange - 8, j * 16 - (int)Main.screenPosition.Y + Main.offScreenRange - 8);
+            Main.tileBatch.Draw(texture, drawPosition, new Rectangle(36 * 6, 36 * 2, 32, 32), vertices, Vector2.Zero, 1f, SpriteEffects.None);
+        }
+
+        void DrawMiddleFrameSegment(Texture2D texture, int i, int j)
+        {
+            Lighting.GetCornerColors(i, j, out VertexColors vertices);
+
+            Vector2 drawPosition = new Vector2(i * 16 - (int)Main.screenPosition.X + Main.offScreenRange - 8, j * 16 - (int)Main.screenPosition.Y + Main.offScreenRange - 8);
+            Main.tileBatch.Draw(texture, drawPosition, new Rectangle(36 * 2, 36 * 1, 32, 32), vertices, Vector2.Zero, 1f, SpriteEffects.None);
+        }
+
+
         public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
         {
             Tile tile = Main.tile[i, j];
             Tile tileLeft = Main.tile[i - 1, j];
             Tile tileRight = Main.tile[i + 1, j];
+            Tile tileAbove = Main.tile[i, j - 1];
             Tile tileBelow = Main.tile[i, j + 1];
 
             Lighting.GetCornerColors(i, j, out VertexColors vertices);
@@ -40,12 +58,61 @@ namespace OvermorrowMod.Content.Tiles.Archives
 
             Texture2D texture = ModContent.Request<Texture2D>(AssetDirectory.ArchiveTiles + "ArchiveBookWallFrame").Value;
             Vector2 drawPosition = new Vector2(i * 16 - (int)Main.screenPosition.X + Main.offScreenRange - 8, j * 16 - (int)Main.screenPosition.Y + Main.offScreenRange - 8);
+
+
+            // Bottom Frame Sections
             if (tileBelow.HasTile)
+            {
+                // End Sections
+                if (tileLeft.WallType == ModContent.WallType<CastleWall>() || tileRight.WallType == ModContent.WallType<CastleWall>())
+                {
+                    Main.tileBatch.Draw(texture, drawPosition, new Rectangle(36 * 1, 36 * 1, 32, 32), vertices, Vector2.Zero, 1f, SpriteEffects.None);
+                    return false;
+                }
+
+                // Middle Frame Sections
+                if (tileLeft.WallType == ModContent.WallType<ArchiveBookWallFrame>() || tileRight.WallType == ModContent.WallType<ArchiveBookWallFrame>())
+                    DrawMiddleFrameSegment(texture, i, j);
+                return false;
+            }
+
+
+            if (tileAbove.WallType == ModContent.WallType<ArchiveBookWall>() && tileAbove.WallType == ModContent.WallType<ArchiveBookWall>())
+            {
+                DrawMiddleFrameSegment(texture, i, j);
+                return false;
+            }
+
+            if (tileLeft.WallType == ModContent.WallType<ArchiveBookWall>() && tileRight.WallType == ModContent.WallType<ArchiveBookWall>())
+            {
+                DrawVerticalFrameSegment(texture, i, j);
+                return false;
+            }
+
+            // Left and Right Frame Sections
+            if ((tileLeft.WallType == ModContent.WallType<ArchiveBookWall>() && tileRight.WallType == ModContent.WallType<CastleWall>()) || 
+                (tileRight.WallType == ModContent.WallType<ArchiveBookWall>() && tileLeft.WallType == ModContent.WallType<CastleWall>()))
+            {
+                DrawVerticalFrameSegment(texture, i, j);
+                return false;
+            }
+
+            // Middle Left/Right End Sections
+            if (tileLeft.WallType == ModContent.WallType<CastleWall>() || tileRight.WallType == ModContent.WallType<CastleWall>())
             {
                 Main.tileBatch.Draw(texture, drawPosition, new Rectangle(36 * 1, 36 * 1, 32, 32), vertices, Vector2.Zero, 1f, SpriteEffects.None);
                 return false;
             }
 
+            // Intersection Segment
+            if (tileLeft.WallType == ModContent.WallType<ArchiveBookWallFrame>() || tileRight.WallType == ModContent.WallType<CastleWall>())
+            {
+                Main.tileBatch.Draw(texture, drawPosition, new Rectangle(36 * 1, 36 * 1, 32, 32), vertices, Vector2.Zero, 1f, SpriteEffects.None);
+                return false;
+            }
+
+
+            // Middle Frame Sections
             if (tileLeft.WallType == ModContent.WallType<ArchiveBookWallFrame>() && tileRight.WallType == ModContent.WallType<ArchiveBookWallFrame>() &&
                 tileBelow.WallType == ModContent.WallType<ArchiveBookWall>())
             {
@@ -54,19 +121,15 @@ namespace OvermorrowMod.Content.Tiles.Archives
             }
 
 
-            if (tileLeft.WallType == ModContent.WallType<ArchiveBookWall>() || tileRight.WallType == ModContent.WallType<ArchiveBookWall>())
-            {
-                Main.tileBatch.Draw(texture, drawPosition, new Rectangle(36 * 6, 36 * 2, 32, 32), vertices, Vector2.Zero, 1f, SpriteEffects.None);
 
-                return false;
-            }
 
-            
+
             if (frame2.Intersects(new Rectangle(36, 36, 36 * 3, 36)))
             {
                 //Main.tileBatch.Draw(ModContent.Request<Texture2D>(AssetDirectory.ArchiveTiles + "ArchiveBookWall").Value, new Vector2(i * 16 - (int)Main.screenPosition.X + Main.offScreenRange - 8, j * 16 - (int)Main.screenPosition.Y + Main.offScreenRange - 8), frame2, vertices, Vector2.Zero, 1f, SpriteEffects.None);
             }
             //Main.tileBatch.Draw(ModContent.Request<Texture2D>(AssetDirectory.ArchiveTiles + "ArchiveBookWall").Value, new Vector2(i * 16 - (int)Main.screenPosition.X + Main.offScreenRange, j * 16 - (int)Main.screenPosition.Y + Main.offScreenRange), frame, vertices, Vector2.Zero, 1f, SpriteEffects.None);
+            Main.tileBatch.Draw(texture, drawPosition, new Rectangle(36 * 1, 36 * 1, 32, 32), vertices, Vector2.Zero, 1f, SpriteEffects.None);
 
             return true;
         }
