3c58a8634eba38a62278f5ea704ee41cd097966d
kjopnfer <gaypal0805@gmail.com>
2024-11-29 22:13:31 -0500

    added test placeable object

---
+++ Content/Items/TestPlaceable.cs
@@ -0,0 +1,34 @@
+
+using OvermorrowMod.Common;
+using OvermorrowMod.Content.Tiles.Archives;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace OvermorrowMod.Content.Items
+{
+    public class TestPlaceable : ModItem
+    {
+        public override string Texture => AssetDirectory.Items + "TowerKey";
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Test Placeable");
+            // Tooltip.SetDefault("'THIS IS A TESTING ITEM'");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 20;
+            Item.maxStack = 99;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = 2000;
+            Item.createTile = TileType<BanquetTable>();
+        }
+    }
+}
