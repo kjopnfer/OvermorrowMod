ce9150be99d4d0e035374d3aa44c56b884fba2a9
kjopnfer <gaypal0805@gmail.com>
2024-12-07 20:07:28 -0500

    added chameleon painting (no idea if it works)

--- Content/Tiles/Archives/MultiTiles/Paintings.cs
+++ Content/Tiles/Archives/MultiTiles/Paintings.cs
@@ -61,4 +61,31 @@ namespace OvermorrowMod.Content.Tiles.Archives
             AddMapEntry(new Color(159, 183, 204));
         }
     }
+
+    public class Chameleon : ModTile
+    {
+        public override string Texture => AssetDirectory.ArchiveTiles + Name;
+        public override bool CanExplode(int i, int j) => false;
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+
+            TileObjectData.newTile.AnchorWall = true;
+
+            TileObjectData.newTile.Width = 4;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.CoordinateHeights = Enumerable.Repeat(16, TileObjectData.newTile.Height).ToArray();
+
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.Origin = new Point16(0, 3);
+
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(164, 146, 100));
+        }
+    }
 }
