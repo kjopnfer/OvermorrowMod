d729cc7e8a2902918fc9cd9f7f50c96083a8033b
kjopnfer <gaypal0805@gmail.com>
2025-01-06 00:02:50 -0500

    move biome from core to content

---
+++ Content/Biomes/GrandArchives.cs
@@ -0,0 +1,34 @@
+using OvermorrowMod.Content.Backgrounds;
+using OvermorrowMod.Core;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.ModLoader;
+
+namespace OvermorrowMod.Content.Biomes
+{
+    public class GrandArchives : ModBiome
+    {
+        // Declare biome priority. The default is BiomeLow so this is only necessary if it needs a higher priority.
+        public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
+        public override ModSurfaceBackgroundStyle SurfaceBackgroundStyle => ModContent.GetInstance<ArchivesBackgroundStyle>();
+        public override ModUndergroundBackgroundStyle UndergroundBackgroundStyle => ModContent.GetInstance<ArchivesUndergroundBackgroundStyle>();
+        public override bool IsBiomeActive(Player player)
+        {
+            return OvermorrowModSystem.ArchiveTiles >= 50;
+        }
+
+        public override void SpecialVisuals(Player player, bool isActive)
+        {
+            if (SkyManager.Instance["OM:ArchiveSky"] != null && isActive != SkyManager.Instance["OM:ArchiveSky"].IsActive())
+            {
+                if (isActive)
+                    SkyManager.Instance.Activate("OM:ArchiveSky");
+                else
+                    SkyManager.Instance.Deactivate("OM:ArchiveSky");
+
+            }
+
+            base.SpecialVisuals(player, isActive);
+        }
+    }
+}
