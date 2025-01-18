44a9c6ae40344138acd687faad7dcd92acac3b70
kjopnfer <gaypal0805@gmail.com>
2025-01-17 22:44:05 -0500

    animated fire trail

--- Content/Trails/FireTrail.cs
+++ Content/Trails/FireTrail.cs
@@ -1,12 +1,102 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace OvermorrowMod.Common.Primitives.Trails
 {
     public class FireTrail : SimpleTrail
     {
-        public FireTrail() : base(22, ModContent.Request<Texture2D>(AssetDirectory.Trails + "Fire").Value, true)
+        public FireTrail() : base(42, ModContent.Request<Texture2D>(AssetDirectory.Trails + "Fire", AssetRequestMode.ImmediateLoad).Value, true)
         {
+        }
+
+        public override void PrepareTrail()
+        {
+            if (Positions.Count < 2) return;
+
+            int segments = 2; // Number of segments for the semicircle
+            float arcRadius = Config.TrailSize(1f); // Radius of the semicircle (based on trail size)
+
+            for (int i = 0; i < Positions.Count - 1; i++)
+            {
+                float prog1 = (float)(i) / (float)Positions.Count;
+                float prog2 = (float)(i + 1) / (float)Positions.Count;
+
+                Vector2 pos1 = Positions[i];
+                Vector2 pos2 = Positions[i + 1];
+
+                if (pos1 == pos2) continue;
+
+                Vector2 off1 = PrimitiveHelper.GetRotation(Positions, i) * Config.TrailSize(prog1) * prog1;
+                Vector2 off2 = PrimitiveHelper.GetRotation(Positions, i + 1) * Config.TrailSize(prog2) * prog2;
+
+                Color col1 = Config.TrailColor(prog1);
+                Color col2 = Config.TrailColor(prog2);
+
+                if (i == Positions.Count - 2) // For the head of the trail
+                {
+                    Vector2 headCenter = pos1; // Use pos1 as the center
+                    Vector2 direction = PrimitiveHelper.GetRotation(Positions, i);
+
+                    for (int j = 0; j < segments; j++)
+                    {
+                        float angle1 = -MathHelper.Pi + j * (MathHelper.Pi / segments);
+                        float angle2 = -MathHelper.Pi + (j + 1) * (MathHelper.Pi / segments);
+
+                        Vector2 arcPoint1 = headCenter + direction.RotatedBy(angle1) * arcRadius;
+                        Vector2 arcPoint2 = headCenter + direction.RotatedBy(angle2) * arcRadius;
+
+                        AddVertex(headCenter, col1, new Vector2(0.5f, 0.5f)); // Center of the semicircle
+                        AddVertex(arcPoint1, col2, new Vector2(0f, 0f));  // First arc point
+                        AddVertex(arcPoint2, col2, new Vector2(0.5f, 0f));  // Second arc point
+                    }
+                }
+                else
+                {
+                    float segmentSize = 1.0f / (float)(Positions.Count - 1);
+                    float startOffset = segmentSize * i;
+
+                    // Compute scrolling offset based on game time
+                    float scrollSpeed = 0.5f; // Adjust the speed of scrolling
+                    float time = (float)Main.GameUpdateCount / 10f; // Replace with your game time logic
+                    float scrollOffset = (time * scrollSpeed) % 1.0f; // Loop the offset when it exceeds 1.0
+
+                    // Adjust texture coordinates with the scrolling offset
+                    float startOffsetScrolled = (startOffset + scrollOffset) % 1.0f;
+                    float endOffsetScrolled = (startOffset + segmentSize + scrollOffset) % 1.0f;
+
+                    AddVertex(pos1 + off1, col1, new Vector2(startOffsetScrolled, 0f));
+                    AddVertex(pos1 - off1, col1, new Vector2(startOffsetScrolled, 1f));
+                    AddVertex(pos2 + off2, col1, new Vector2(endOffsetScrolled, 0f));
+
+                    AddVertex(pos2 + off2, col1, new Vector2(endOffsetScrolled, 0f));
+                    AddVertex(pos2 - off2, col1, new Vector2(endOffsetScrolled, 1f));
+                    AddVertex(pos1 - off1, col1, new Vector2(startOffsetScrolled, 1f));
+
+                    //AddVertex(pos1 + off1, col1, new Vector2(prog1 + offset, 1));
+                    //AddVertex(pos1 - off1, col1, new Vector2(prog1 + offset, 0));
+                    //AddVertex(pos2 + off2, col1, new Vector2(prog1 + offset, 1));
+
+                    //AddVertex(pos2 + off2, col1, new Vector2(prog2 + offset, 1));
+                    //AddVertex(pos2 - off2, col1, new Vector2(prog2 + offset, 0));
+                    //AddVertex(pos1 - off1, col1, new Vector2(prog2 + offset, 0));
+
+                    //AddVertex(pos1 + off1, col1, new Vector2(0, 0));
+                    //AddVertex(pos1 - off1, col1, new Vector2(0, 1));
+                    //AddVertex(pos2 + off2, col1, new Vector2(1, 0));
+
+                    //AddVertex(pos2 + off2, col1, new Vector2(1, 0));
+                    //AddVertex(pos2 - off2, col1, new Vector2(1, 1));
+                    //AddVertex(pos1 - off1, col1, new Vector2(0, 1));
+                }
+            }
+        }
+
+        public override void UpdateDead()
+        {
+            Dead = true;
         }
     }
 }
