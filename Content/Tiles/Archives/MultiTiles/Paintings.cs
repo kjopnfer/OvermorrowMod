d503b7ced64378f9017de7def4a25860c0477d7f
kjopnfer <gaypal0805@gmail.com>
2025-06-12 19:24:56 -0400

    added paintings

--- Content/Tiles/Archives/MultiTiles/Paintings.cs
+++ Content/Tiles/Archives/MultiTiles/Paintings.cs
@@ -8,6 +8,35 @@ using Terraria.ObjectData;
 
 namespace OvermorrowMod.Content.Tiles.Archives
 {
+    public abstract class Painting : ModTile
+    {
+        public override string Texture => AssetDirectory.ArchiveTiles + Name;
+        public override bool CanExplode(int i, int j) => false;
+        public abstract int Width { get; }
+        public abstract int Height { get; }
+        public abstract Color MapColor { get; }
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+
+            TileObjectData.newTile.AnchorWall = true;
+
+            TileObjectData.newTile.Width = Width;
+            TileObjectData.newTile.Height = Height;
+            TileObjectData.newTile.CoordinateHeights = Enumerable.Repeat(16, TileObjectData.newTile.Height).ToArray();
+
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.Origin = new Point16(0, Height - 1);
+
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+
+            TileObjectData.addTile(Type);
+            AddMapEntry(MapColor);
+        }
+    }
+
     public class Napoleon : ModTile
     {
         public override string Texture => AssetDirectory.ArchiveTiles + Name;
@@ -62,30 +91,31 @@ namespace OvermorrowMod.Content.Tiles.Archives
         }
     }
 
-    public class Chameleon : ModTile
+    public class Chameleon : Painting
     {
-        public override string Texture => AssetDirectory.ArchiveTiles + Name;
-        public override bool CanExplode(int i, int j) => false;
-        public override void SetStaticDefaults()
-        {
-            Main.tileFrameImportant[Type] = true;
-            Main.tileNoAttach[Type] = true;
+        public override int Width => 4;
+        public override int Height => 4;
+        public override Color MapColor => new Color(164, 146, 100);
+    }
 
-            TileObjectData.newTile.AnchorWall = true;
+    public class ABriefRespite : Painting
+    {
+        public override int Width => 6;
+        public override int Height => 4;
+        public override Color MapColor => new Color(85, 42, 18);
+    }
 
-            TileObjectData.newTile.Width = 4;
-            TileObjectData.newTile.Height = 4;
-            TileObjectData.newTile.CoordinateHeights = Enumerable.Repeat(16, TileObjectData.newTile.Height).ToArray();
+    public class Selfie : Painting
+    {
+        public override int Width => 6;
+        public override int Height => 7;
+        public override Color MapColor => new Color(85, 42, 18);
+    }
 
-            TileObjectData.newTile.UsesCustomCanPlace = true;
-            TileObjectData.newTile.Origin = new Point16(0, 3);
-
-            TileObjectData.newTile.CoordinateWidth = 16;
-            TileObjectData.newTile.CoordinatePadding = 2;
-
-            TileObjectData.addTile(Type);
-
-            AddMapEntry(new Color(164, 146, 100));
-        }
+    public class GodsIris : Painting
+    {
+        public override int Width => 8;
+        public override int Height => 4;
+        public override Color MapColor => new Color(85, 42, 18);
     }
 }
