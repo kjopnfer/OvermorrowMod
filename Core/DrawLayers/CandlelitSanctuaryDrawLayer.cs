aac519e3efe895be42b24d218558c8a09a07fd27
kjopnfer <gaypal0805@gmail.com>
2025-06-22 16:23:23 -0400

    fix candlelit sanctuary flame being wrong when flipped gravity

--- Core/DrawLayers/CandlelitSanctuaryDrawLayer.cs
+++ Core/DrawLayers/CandlelitSanctuaryDrawLayer.cs
@@ -39,30 +39,7 @@ namespace OvermorrowMod.Core.DrawLayers
             int direction = drawPlayer.direction;
             Vector2 velocity = -Vector2.UnitY * 0.8f;
 
-            // Helper function to spawn a particle at an offset
-            void SpawnParticles(Vector2 offset)
-            {
-                Vector2 position = drawPlayer.Center + offset;
-                //    Particle.CreateParticleDirect(Particle.ParticleType<Ember>(), position, velocity, new Color(255, 238, 241), 1f, scale * 0.5f, 0f, 0, scale * 0.5f);
-            }
-
-            // Always spawn the first particle
-            /*SpawnParticles(new Vector2(6 * direction, -8));
-
-
-            // Additional particles based on charge count
-            if (charges >= 2)
-            {
-                SpawnParticles(new Vector2(18 * direction, -12));
-            }
-
-            if (charges >= 3)
-            {
-                SpawnParticles(new Vector2(12 * direction, -14));
-            }*/
-
-            //CandleFlame = ModContent.Request<Texture2D>(AssetDirectory.ArchiveItems + "CandlelitSanctuaryFlame", ReLogic.Content.AssetRequestMode.ImmediateLoad);
-            var shieldMask = CandleFlame;
+             var shieldMask = CandleFlame;
             Vector2 Position = drawInfo.Position;
             Vector2 origin = new(CandleFlame.Value.Width * 0.5f, CandleFlame.Value.Height * 0.5f);
             Vector2 drawPos = new Vector2((int)(Position.X - drawPlayer.bodyFrame.Width / 2 + drawPlayer.width / 2), (int)(Position.Y + drawPlayer.height - drawPlayer.bodyFrame.Height + 4f)) + drawPlayer.bodyPosition + new Vector2(drawPlayer.bodyFrame.Width / 2, drawPlayer.bodyFrame.Height / 2);
@@ -85,8 +62,9 @@ namespace OvermorrowMod.Core.DrawLayers
 
             int[] upFrames = [7, 8, 9, 14, 15, 16];
 
-            float jumpOffset = movementFrame == 5 ? -8 : 0;
-            float walkOffset = upFrames.Contains(movementFrame) ? -2 : 0;
+            float jumpOffset = movementFrame == 5 ? -8 * drawPlayer.gravDir : 0;
+
+            float walkOffset = upFrames.Contains(movementFrame) ? -2 * drawPlayer.gravDir : 0;
             for (int i = 0; i < charges; i++)
             {
                 //SpawnParticles(new Vector2(12 * direction, -14));
@@ -94,18 +72,19 @@ namespace OvermorrowMod.Core.DrawLayers
                 Vector2 chargeOffset = Vector2.Zero;
                 if (i == 1)
                 {
-                    chargeOffset = new Vector2(12, -4);
+                    chargeOffset = new Vector2(12, -4 * drawPlayer.gravDir);
                 }
                 else if (i == 2)
                 {
-                    chargeOffset = new Vector2(6, -6);
+                    chargeOffset = new Vector2(6, -6 * drawPlayer.gravDir);
                 }
 
+                Vector2 gravityOffset = drawPlayer.gravDir == -1 ? new Vector2(0, 42) : Vector2.Zero;
                 Vector2 position = new Vector2((14 + chargeOffset.X) * direction, -10 + chargeOffset.Y + jumpOffset + walkOffset) + new Vector2((int)(drawInfo.Position.X - Main.screenPosition.X - bodyFrame.Width / 2 + drawInfo.drawPlayer.width / 2), (int)(drawInfo.Position.Y - Main.screenPosition.Y + drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height + 4f)) + drawInfo.drawPlayer.bodyPosition + new Vector2(bodyFrame.Width / 2, drawInfo.drawPlayer.bodyFrame.Height / 2);
                 var frameOffset = (int)MathHelper.Lerp(0, 5, ((Main.GlobalTimeWrappedHourly + (0.15f * i)) % 0.6f) / 0.6f);
                 var frame = new Rectangle(0, (int)(shieldMask.Height() / 5 * frameOffset), shieldMask.Width(), shieldMask.Height() / 5);
 
-                DrawData drawData = new(shieldMask.Value, position, frame, Color.White, drawInfo.drawPlayer.bodyRotation, bodyVect, 0.7f, drawInfo.playerEffect, 1)
+                DrawData drawData = new(shieldMask.Value, position + gravityOffset, frame, Color.White, drawInfo.drawPlayer.bodyRotation, bodyVect, 0.7f, drawInfo.playerEffect, 1)
                 {
                     shader = drawInfo.cShield
                 };
