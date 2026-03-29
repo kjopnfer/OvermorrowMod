6f64cb866ad6d7190c76a8adbdf2d0643280c5a5
kjopnfer <gaypal0805@gmail.com>
2026-03-28 23:43:52 -0400

    GAP WOOD GAP STONE GAP

--- Core/WorldGeneration/Procedural/Templates/Connectors/FlatCorridor.cs
+++ Core/WorldGeneration/Procedural/Templates/Connectors/FlatCorridor.cs
@@ -1,36 +1,64 @@
 using Microsoft.Xna.Framework;
+using OvermorrowMod.Content.Tiles.Archives;
 using Terraria;
-using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace OvermorrowMod.Core.WorldGeneration.Procedural.Templates.Connectors
 {
     public class FlatCorridor : IProcedural
     {
         private const int CorridorHeight = 8;
-        private const int Length = 41;
+
+        int[] widths = { 1, 1, 2, 1, 4, 1, 2, 1, 5, 1, 2, 1, 4, 1, 2, 1};
+        int[] types =  {-1, -1, 0,-1, 1,-1, 0,-1, 2,-1, 0,-1, 1,-1, 0,-1};
+        // 0 = wood, 1 = castle/stone, 2 = blue, -1 = gap
 
         public SocketAnchor Build(SocketAnchor entry, int fillTileType, int liningTileType)
         {
             int startX = entry.Position.X;
             int floorY = entry.Position.Y;
 
-            WorldGen.PlaceTile(startX, floorY - CorridorHeight, TileID.Adamantite, true, true);
-            WorldGen.PlaceTile(startX, floorY, TileID.Cobalt, true, true);
+            int length = 0;
+            for (int i = 0; i < widths.Length; i++) length += widths[i];
 
-            // Clear the corridor air space
-            for (int x = startX; x <= startX + Length; x++)
-                for (int y = floorY - CorridorHeight; y <= floorY; y++)
+            int endX = startX + length;
+            int ceilingY = floorY - CorridorHeight;
+
+            // Clear remove tiles
+            for (int x = startX; x <= endX; x++)
+                for (int y = ceilingY; y <= floorY; y++)
                     WorldGen.KillTile(x, y);
 
-            int endX = startX + Length;
-            WorldGen.PlaceTile(endX, floorY - CorridorHeight, TileID.Orichalcum, true, true);
-            WorldGen.PlaceTile(endX, floorY, TileID.Mythril, true, true);
+            int woodWall = ModContent.WallType<ArchiveWoodWall>();
+            int castleWall = ModContent.WallType<CastleWall>();
+            int blueWall = ModContent.WallType<ArchiveWoodWallBlue>();
+            int wallTop = ceilingY + 1;
+            int wallBottom = floorY - 1;
+
+            int cursor = startX;
+
+            for (int i = 0; i < widths.Length; i++)
+            {
+                if (types[i] >= 0)
+                {
+                    int wallType = types[i] switch { 0 => woodWall, 1 => castleWall, 2 => blueWall, _ => woodWall };
+                    PlaceWallStripe(cursor, wallTop, widths[i], wallBottom, wallType);
+                }
+                cursor += widths[i];
+            }
 
             return new SocketAnchor
             {
                 Position = new Point(endX, floorY),
                 Facing = SocketDirection.Right
             };
+        }
+
+        private static void PlaceWallStripe(int startX, int top, int width, int bottom, int wallType)
+        {
+            for (int x = startX; x < startX + width; x++)
+                for (int y = top; y <= bottom; y++)
+                    WorldGen.PlaceWall(x, y, wallType, true);
         }
     }
 }
